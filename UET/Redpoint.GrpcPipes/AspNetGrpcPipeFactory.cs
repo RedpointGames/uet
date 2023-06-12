ab9773a5c88050c8638fd4ef45688655f052b899
June Rhodes <jrhodes@redpoint.games>
2023-06-12 12:46:59 +1000

    Try to fix pipe paths being too long

--- UET/Redpoint.GrpcPipes/AspNetGrpcPipeFactory.cs
+++ UET/Redpoint.GrpcPipes/AspNetGrpcPipeFactory.cs
@@ -6,14 +6,68 @@
 
     internal class AspNetGrpcPipeFactory : IGrpcPipeFactory
     {
-        internal readonly string _basePath =
-            Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                ".redpoint-grpc-sockets");
+        private string GetBasePath()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                var path = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                    ".redpoint-grpc-pipes",
+                    Environment.UserName);
+                Directory.CreateDirectory(path);
+                return path;
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                var rootPath = "/Users/Shared/.redpoint-grpc-pipes";
+                Directory.CreateDirectory(rootPath);
+                File.SetUnixFileMode(
+                    rootPath,
+                    UnixFileMode.UserRead |
+                    UnixFileMode.UserWrite |
+                    UnixFileMode.UserExecute |
+                    UnixFileMode.GroupRead |
+                    UnixFileMode.GroupWrite |
+                    UnixFileMode.GroupExecute |
+                    UnixFileMode.OtherRead |
+                    UnixFileMode.OtherWrite |
+                    UnixFileMode.OtherExecute);
+                var path = Path.Combine(
+                    rootPath,
+                    Environment.UserName);
+                Directory.CreateDirectory(path);
+                return path;
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                var rootPath = "/tmp/.redpoint-grpc-pipes";
+                Directory.CreateDirectory(rootPath);
+                File.SetUnixFileMode(
+                    rootPath,
+                    UnixFileMode.UserRead |
+                    UnixFileMode.UserWrite |
+                    UnixFileMode.UserExecute |
+                    UnixFileMode.GroupRead |
+                    UnixFileMode.GroupWrite |
+                    UnixFileMode.GroupExecute |
+                    UnixFileMode.OtherRead |
+                    UnixFileMode.OtherWrite |
+                    UnixFileMode.OtherExecute);
+                var path = Path.Combine(
+                    rootPath,
+                    Environment.UserName);
+                Directory.CreateDirectory(path);
+                return path;
+            }
+            else
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
 
         public IGrpcPipeServer<T> CreateServer<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicMethods | DynamicallyAccessedMemberTypes.NonPublicMethods)] T>(string pipeName, T instance) where T : class
         {
-            return new AspNetGrpcPipeServer<T>(Path.Combine(_basePath, pipeName), instance);
+            return new AspNetGrpcPipeServer<T>(Path.Combine(GetBasePath(), pipeName), instance);
         }
 
         public T CreateClient<T>(string pipeName, Func<GrpcChannel, T> constructor)
@@ -27,7 +81,7 @@
                         SocketType.Stream,
                         ProtocolType.Unspecified);
                     await socket.ConnectAsync(
-                        new UnixDomainSocketEndPoint(Path.Combine(_basePath, pipeName)),
+                        new UnixDomainSocketEndPoint(Path.Combine(GetBasePath(), pipeName)),
                         cancellationToken);
                     return new NetworkStream(socket, true);
                 }
