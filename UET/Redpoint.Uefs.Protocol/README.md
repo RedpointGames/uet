41edf7bc60cf81fb7fccc119061139a4408b8086
June Rhodes <jrhodes@redpoint.games>
2023-06-14 20:16:13 +1000

    Add Redpoint.Uefs.Protocol library and publish it

---
+++ UET/Redpoint.Uefs.Protocol/README.md
@@ -0,0 +1,3 @@
+# Redpoint.Uefs.Protocol
+
+This library provides the gRPC protocol specification for the UEFS service, plus dependency injection bindings to create a UEFS client.
---
+++ UET/Redpoint.Uefs.Protocol/UefsServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Redpoint.Uefs.Protocol
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Redpoint.GrpcPipes;
+
+    /// <summary>
+    /// Provides <see cref="AddUefs(IServiceCollection)"/> to add the UEFS gRPC client to dependency injection.
+    /// </summary>
+    public static class UefsServiceExtensions
+    {
+        /// <summary>
+        /// Registers the <see cref="Uefs.UefsClient"/> service with dependency injection.
+        /// </summary>
+        /// <param name="services">The service collection to add the <see cref="Uefs.UefsClient"/> service to.</param>
+        public static void AddUefs(this IServiceCollection services)
+        {
+            services.AddSingleton(sp =>
+            {
+                var factory = sp.GetRequiredService<IGrpcPipeFactory>();
+                return factory.CreateClient(
+                    "UEFS",
+                    GrpcPipeNamespace.Computer,
+                    channel => new Uefs.UefsClient(channel));
+            });
+        }
+    }
+}
