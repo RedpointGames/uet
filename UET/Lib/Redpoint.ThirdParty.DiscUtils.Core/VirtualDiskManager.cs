fae11cb6c633a5c277c2c524d8ea285d68d8de8e
June Rhodes <jrhodes@redpoint.games>
2023-06-25 03:00:04 +1000

    Import DiscUtils libraries and target .NET 7

---
+++ UET/Lib/Redpoint.ThirdParty.DiscUtils.Core/VirtualDiskManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using DiscUtils.CoreCompat;
+using DiscUtils.Internal;
+
+namespace DiscUtils
+{
+    /// <summary>
+    /// Helps discover and use VirtualDiskFactory's
+    /// </summary>
+    public static class VirtualDiskManager
+    {
+        static VirtualDiskManager()
+        {
+            ExtensionMap = new Dictionary<string, VirtualDiskFactory>();
+            TypeMap = new Dictionary<string, VirtualDiskFactory>();
+            DiskTransports = new Dictionary<string, Type>();
+        }
+
+        internal static Dictionary<string, Type> DiskTransports { get; }
+        internal static Dictionary<string, VirtualDiskFactory> ExtensionMap { get; }
+
+        /// <summary>
+        /// Gets the set of disk formats supported as an array of file extensions.
+        /// </summary>
+        public static ICollection<string> SupportedDiskFormats
+        {
+            get { return ExtensionMap.Keys; }
+        }
+
+        /// <summary>
+        /// Gets the set of disk types supported, as an array of identifiers.
+        /// </summary>
+        public static ICollection<string> SupportedDiskTypes
+        {
+            get { return TypeMap.Keys; }
+        }
+
+        internal static Dictionary<string, VirtualDiskFactory> TypeMap { get; }
+
+        /// <summary>
+        /// Locates VirtualDiskFactory factories attributed with VirtualDiskFactoryAttribute, and types marked with VirtualDiskTransportAttribute, that are able to work with Virtual Disk types.
+        /// </summary>
+        /// <param name="assembly">An assembly to scan</param>
+        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "The types are marked with DynamicallyAccessedMembers.All")]
+        [SuppressMessage("Trimming", "IL2072:Target parameter argument does not satisfy 'DynamicallyAccessedMembersAttribute' in call to target method. The return value of the source method does not have matching annotations.", Justification = "The types are marked with DynamicallyAccessedMembers.All")]
+        public static void RegisterVirtualDiskTypes(Assembly assembly)
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                VirtualDiskFactoryAttribute diskFactoryAttribute = (VirtualDiskFactoryAttribute)ReflectionHelper.GetCustomAttribute(type, typeof(VirtualDiskFactoryAttribute), false);
+                if (diskFactoryAttribute != null)
+                {
+                    VirtualDiskFactory factory = (VirtualDiskFactory)Activator.CreateInstance(type);
+                    TypeMap.Add(diskFactoryAttribute.Type, factory);
+
+                    foreach (string extension in diskFactoryAttribute.FileExtensions)
+                    {
+                        ExtensionMap.Add(extension.ToUpperInvariant(), factory);
+                    }
+                }
+
+                VirtualDiskTransportAttribute diskTransportAttribute = ReflectionHelper.GetCustomAttribute(type, typeof(VirtualDiskTransportAttribute), false) as VirtualDiskTransportAttribute;
+                if (diskTransportAttribute != null)
+                {
+                    DiskTransports.Add(diskTransportAttribute.Scheme.ToUpperInvariant(), type);
+                }
+            }
+        }
+    }
+}
---
+++ UET/Lib/Redpoint.ThirdParty.DiscUtils.Ntfs/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+#if NET5_0_OR_GREATER
+using System.Runtime.Versioning;
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+#if NET5_0_OR_GREATER
+[assembly: SupportedOSPlatform("windows")]
+#endif
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("9095333e-08e4-4c13-b556-20b5fdb9db2a")]
