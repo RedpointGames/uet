7794de3937b2b0eb62d24373346f00f3056e7e34
June Rhodes <jrhodes@redpoint.games>
2023-06-27 22:00:08 +1000

    Implement Backblaze B2 uploading

--- UET/Redpoint.UET.BuildPipeline.Providers.Deployment/Plugin/BackblazeB2/BackblazeB2PluginDeploymentProvider.cs
+++ UET/Redpoint.UET.BuildPipeline.Providers.Deployment/Plugin/BackblazeB2/BackblazeB2PluginDeploymentProvider.cs
@@ -1,8 +1,9 @@
 ﻿namespace Redpoint.UET.BuildPipeline.Providers.Deployment.Plugin.BackblazeB2
 {
+    using Redpoint.UET.BuildGraph;
+    using Redpoint.UET.Configuration;
     using Redpoint.UET.Configuration.Dynamic;
     using Redpoint.UET.Configuration.Plugin;
-    using System;
     using System.Collections.Generic;
     using System.Text.Json;
     using System.Threading.Tasks;
@@ -10,6 +11,14 @@
 
     internal class BackblazeB2PluginDeploymentProvider : IPluginDeploymentProvider
     {
+        private readonly IGlobalArgsProvider? _globalArgsProvider;
+
+        public BackblazeB2PluginDeploymentProvider(
+            IGlobalArgsProvider? globalArgsProvider = null)
+        {
+            _globalArgsProvider = globalArgsProvider;
+        }
+
         public string Type => "BackblazeB2";
 
         public object DeserializeDynamicSettings(ref Utf8JsonReader reader, JsonSerializerOptions options)
@@ -17,9 +26,54 @@
             return JsonSerializer.Deserialize(ref reader, DeploymentProviderSourceGenerationContext.WithStringEnum.BuildConfigPluginDeploymentBackblazeB2)!;
         }
 
-        public Task WriteBuildGraphNodesAsync(IBuildGraphEmitContext context, XmlWriter writer, BuildConfigPluginDistribution buildConfigDistribution, IEnumerable<BuildConfigDynamic<BuildConfigPluginDistribution, IDeploymentProvider>> elements)
+        public async Task WriteBuildGraphNodesAsync(
+            IBuildGraphEmitContext context,
+            XmlWriter writer,
+            BuildConfigPluginDistribution buildConfigDistribution,
+            IEnumerable<BuildConfigDynamic<BuildConfigPluginDistribution, IDeploymentProvider>> dynamicSettings)
         {
-            throw new NotImplementedException();
+            var castedSettings = dynamicSettings
+                .Select(x => (name: x.Name, settings: (BuildConfigPluginDeploymentBackblazeB2)x.DynamicSettings))
+                .ToList();
+
+            // Emit the nodes to run each deployment.
+            foreach (var deployment in castedSettings)
+            {
+                await writer.WriteAgentAsync(
+                    new AgentElementProperties
+                    {
+                        Name = $"Deployment {deployment.name}",
+                        Type = "Win64",
+                    },
+                    async writer =>
+                    {
+                        await writer.WriteNodeAsync(
+                            new NodeElementProperties
+                            {
+                                Name = $"Deployment {deployment.name}",
+                                Requires = "#PackagedZip",
+                            },
+                            async writer =>
+                            {
+                                await writer.WriteSpawnAsync(
+                                    new SpawnElementProperties
+                                    {
+                                        Exe = "$(UETPath)",
+                                        Arguments = (_globalArgsProvider?.GlobalArgsArray ?? new string[0]).Concat(new[]
+                                        {
+                                            "internal",
+                                            "upload-to-backblaze-b2",
+                                            "--zip-path",
+                                            $@"""$(ProjectRoot)/$(PluginName)-$(Distribution)-$(VersionName).zip""",
+                                            "--bucket-name",
+                                            $@"""{deployment.settings.BucketName}""",
+                                            "--folder-env-var",
+                                            $@"""{deployment.settings.FolderPrefixEnvVar}"""
+                                        }).ToArray()
+                                    });
+                            });
+                    });
+            }
         }
     }
 }
--- UET/UET/Commands/Internal/UploadToBackblazeB2/UploadToBackblazeB2Command.cs
+++ UET/UET/Commands/Internal/UploadToBackblazeB2/UploadToBackblazeB2Command.cs
@@ -1,6 +1,7 @@
 ﻿namespace UET.Commands.Internal.UploadToBackblazeB2
 {
     using Microsoft.Extensions.Logging;
+    using Redpoint.ProgressMonitor;
     using System.CommandLine;
     using System.CommandLine.Invocation;
     using System.Threading.Tasks;
@@ -9,17 +10,15 @@
     {
         internal class Options
         {
-            public Option<string> ZipPath;
+            public Option<FileInfo> ZipPath;
             public Option<string> BucketName;
             public Option<string> FolderEnvVar;
-            public Option<bool> Marketplace;
 
             public Options()
             {
-                ZipPath = new Option<string>("--zip-path");
-                BucketName = new Option<string>("--bucket-name");
-                FolderEnvVar = new Option<string>("--folder-env-var");
-                Marketplace = new Option<bool>("--marketplace");
+                ZipPath = new Option<FileInfo>("--zip-path") { IsRequired = true };
+                BucketName = new Option<string>("--bucket-name") { IsRequired = true };
+                FolderEnvVar = new Option<string>("--folder-env-var") { IsRequired = true };
             }
         }
 
@@ -35,17 +34,102 @@
         private class UploadToBackblazeB2CommandInstance : ICommandInstance
         {
             private readonly ILogger<UploadToBackblazeB2CommandInstance> _logger;
+            private readonly Options _options;
+            private readonly IMonitorFactory _monitorFactory;
+            private readonly IProgressFactory _progressFactory;
 
             public UploadToBackblazeB2CommandInstance(
-                ILogger<UploadToBackblazeB2CommandInstance> logger)
+                ILogger<UploadToBackblazeB2CommandInstance> logger,
+                Options options,
+                IMonitorFactory monitorFactory,
+                IProgressFactory progressFactory)
             {
                 _logger = logger;
+                _options = options;
+                _monitorFactory = monitorFactory;
+                _progressFactory = progressFactory;
             }
 
-            public Task<int> ExecuteAsync(InvocationContext context)
+            public async Task<int> ExecuteAsync(InvocationContext context)
             {
-                _logger.LogError("Not yet implemented.");
-                return Task.FromResult(1);
+                var zipPath = context.ParseResult.GetValueForOption(_options.ZipPath)!;
+                var bucketName = context.ParseResult.GetValueForOption(_options.BucketName)!;
+                var folderEnvVar = context.ParseResult.GetValueForOption(_options.FolderEnvVar)!;
+
+                if (!zipPath.Exists)
+                {
+                    _logger.LogError("ZIP file to upload does not exist.");
+                    return 1;
+                }
+
+                var folderPrefix = Environment.GetEnvironmentVariable(folderEnvVar);
+                if (string.IsNullOrWhiteSpace(folderPrefix))
+                {
+                    _logger.LogError("Backblaze B2 environment variable for folder prefix missing. Unable to run upload step.");
+                    return 1;
+                }
+
+                var b2KeyId = Environment.GetEnvironmentVariable("DL_BACKBLAZE_B2_KEY_ID");
+                var b2AppKey = Environment.GetEnvironmentVariable("DL_BACKBLAZE_B2_APPLICATION_KEY");
+                if (string.IsNullOrWhiteSpace(b2KeyId) || string.IsNullOrWhiteSpace(b2AppKey))
+                {
+                    _logger.LogError("Backblaze B2 environment variables for authentication missing (expected DL_BACKBLAZE_B2_KEY_ID and DL_BACKBLAZE_B2_APPLICATION_KEY). Unable to run upload step.");
+                    return 1;
+                }
+
+                var client = new B2Net.B2Client(new B2Net.Models.B2Options
+                {
+                    KeyId = b2KeyId,
+                    ApplicationKey = b2AppKey,
+                });
+
+                var bucket = (await client.Buckets.GetList(context.GetCancellationToken())).FirstOrDefault(
+                    x => x.BucketName == bucketName);
+                if (bucket == null)
+                {
+                    _logger.LogError($"Unable to find bucket named '{bucketName}' (maybe you can't access it with this key?)");
+                    return 1;
+                }
+
+                var uploadUrl = await client.Files.GetUploadUrl(bucket.BucketId, context.GetCancellationToken());
+
+                using (var stream = new FileStream(
+                    zipPath.FullName,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read | FileShare.Delete))
+                {
+                    // Start monitoring.
+                    var cts = new CancellationTokenSource();
+                    var progress = _progressFactory.CreateProgressForStream(stream);
+                    var monitorTask = Task.Run(async () =>
+                    {
+                        var monitor = _monitorFactory.CreateByteBasedMonitor();
+                        await monitor.MonitorAsync(
+                            progress,
+                            SystemConsole.ConsoleInformation,
+                            SystemConsole.WriteProgressToConsole,
+                            cts.Token);
+                    });
+
+                    // Upload the file.
+                    await client.Files.Upload(
+                        fileDataWithSHA: stream,
+                        fileName: $"{folderPrefix}/{zipPath.Name}",
+                        uploadUrl: uploadUrl,
+                        contentType: "application/zip",
+                        autoRetry: false,
+                        bucketId: bucket.BucketId,
+                        fileInfo: null,
+                        dontSHA: true,
+                        context.GetCancellationToken());
+
+                    // Stop monitoring.
+                    await SystemConsole.CancelAndWaitForConsoleMonitoringTaskAsync(monitorTask, cts);
+                }
+
+                _logger.LogInformation("Successfully uploaded file to Backblaze B2.");
+                return 0;
             }
         }
     }
