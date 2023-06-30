56c4a03b749e0046d9dc06c50cb503fb9b5fc965
June Rhodes <jrhodes@redpoint.games>
2023-07-01 01:12:14 +1000

    Fix HttpClientFactory incorrectly caching HttpClient instances with different timeouts

--- UET/Lib/Redpoint.ThirdParty.B2Net/Http/HttpClientFactory.cs
+++ UET/Lib/Redpoint.ThirdParty.B2Net/Http/HttpClientFactory.cs
@@ -6,23 +6,16 @@
 
     public static class HttpClientFactory
     {
-        private static HttpClient _client;
-
         public static HttpClient CreateHttpClient(TimeSpan timeout)
         {
-            var client = _client;
-            if (client == null)
-            {
-                var handler = new HttpClientHandler() { AllowAutoRedirect = true };
+            var handler = new HttpClientHandler() { AllowAutoRedirect = true };
 
-                client = new HttpClient(handler);
+            var client = new HttpClient(handler);
 
-                client.Timeout = timeout;
+            client.Timeout = timeout;
 
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                _client = client;
-            }
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             return client;
         }
     }
