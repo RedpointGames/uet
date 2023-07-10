e5ea0ad4c9f001fcfdd6d166b94e06d842a4112e
June Rhodes <jrhodes@redpoint.games>
2023-07-10 16:26:39 +1000

    Try to fix test exclusion for functional tests

---
+++ UET/uet.FunctionalTests/Dummy.cs
@@ -0,0 +1,14 @@
+﻿namespace uet.DummyTests
+{
+    /// <remarks>
+    /// This test class exists so that when we filter out functional tests on the build server,
+    /// it doesn't cause the tests to fail due to no tests matching the filter.
+    /// </remarks>
+    public class Dummy
+    {
+        [Fact]
+        public void DummyTest()
+        {
+        }
+    }
+}
