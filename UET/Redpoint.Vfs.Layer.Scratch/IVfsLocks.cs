953d2c910b7b4145b6cf141d3ea6be65e2a48f40
June Rhodes <jrhodes@redpoint.games>
2023-07-19 05:05:45 +1000

    Replace KeyedSemaphoresCollection with concurrent dictionary of SemaphoreSlim
    KeyedSemaphoresCollection seems to be buggy for the ScratchVfsLayer

---
+++ UET/Redpoint.Vfs.Layer.Scratch/IVfsLocks.cs
@@ -0,0 +1,7 @@
+﻿namespace Redpoint.Vfs.Layer.Scratch
+{
+    internal interface IVfsLocks
+    {
+        bool TryLock(string context, string normalizedKeyPath, TimeSpan timeout, Action callback, out string blockingContext);
+    }
+}
---
+++ UET/Redpoint.Vfs.Layer.Scratch/SemaphoreSlimVfsLocks.cs
@@ -0,0 +1,41 @@
+﻿namespace Redpoint.Vfs.Layer.Scratch
+{
+    using System.Collections.Concurrent;
+
+    internal class SemaphoreSlimVfsLocks : IVfsLocks
+    {
+        private readonly ConcurrentDictionary<string, SemaphoreSlim> _locks;
+        private readonly ConcurrentDictionary<string, string> _lockHolders;
+
+        public SemaphoreSlimVfsLocks()
+        {
+            _locks = new ConcurrentDictionary<string, SemaphoreSlim>();
+            _lockHolders = new ConcurrentDictionary<string, string>();
+        }
+
+        public bool TryLock(string context, string normalizedKeyPath, TimeSpan timeout, Action callback, out string blockingContext)
+        {
+            blockingContext = "(unknown)";
+            var semaphore = _locks.GetOrAdd(normalizedKeyPath, new SemaphoreSlim(1));
+            if (semaphore.Wait(timeout == TimeSpan.MaxValue ? -1 : (int)timeout.TotalMilliseconds))
+            {
+                try
+                {
+                    _lockHolders.AddOrUpdate(normalizedKeyPath, context, (_, _) => context);
+                    callback();
+                    _lockHolders.AddOrUpdate(normalizedKeyPath, "(none)", (_, _) => "(none)");
+                    return true;
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }
+            else
+            {
+                _lockHolders.TryGetValue(normalizedKeyPath, out blockingContext!);
+                return false;
+            }
+        }
+    }
+}
