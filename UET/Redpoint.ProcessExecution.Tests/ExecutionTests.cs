7bf7f848f2ac2f550af7bafb4a9ecfe9dd2208e7
June Rhodes <jrhodes@redpoint.games>
2023-08-16 01:00:52 +1000

    Use P/Invoke for process execution on Windows
    This will allow us to set up chroot'ing for processes in the future.

---
+++ UET/Redpoint.ProcessExecution.Tests/ExecutionTests.cs
@@ -0,0 +1,149 @@
+﻿namespace Redpoint.ProcessExecution.Tests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Text;
+    using Xunit;
+
+    public class ExecutionTests
+    {
+        [SkippableFact]
+        public async Task CanExecuteCmdAsync()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddProcessExecution();
+            var sp = services.BuildServiceProvider();
+
+            var executor = sp.GetRequiredService<IProcessExecutor>();
+
+            var exitCode = await executor.ExecuteAsync(
+                new ProcessSpecification
+                {
+                    FilePath = @"C:\Windows\system32\cmd.exe",
+                    Arguments = new[]
+                    {
+                        "/C",
+                        "echo test",
+                    },
+                },
+                CaptureSpecification.Passthrough,
+                CancellationToken.None);
+            Assert.Equal(0, exitCode);
+        }
+
+        [SkippableFact]
+        public async Task CanCaptureCmdAsync()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddProcessExecution();
+            var sp = services.BuildServiceProvider();
+
+            var executor = sp.GetRequiredService<IProcessExecutor>();
+
+            var stdout = new StringBuilder();
+            var exitCode = await executor.ExecuteAsync(
+                new ProcessSpecification
+                {
+                    FilePath = @"C:\Windows\system32\cmd.exe",
+                    Arguments = new[]
+                    {
+                        "/C",
+                        "echo test",
+                    },
+                },
+                CaptureSpecification.CreateFromStdoutStringBuilder(stdout),
+                CancellationToken.None);
+            Assert.Equal(0, exitCode);
+            Assert.Equal("test", stdout.ToString().Trim());
+        }
+
+        [SkippableFact]
+        public async Task CanGetExitCodeOneFromCmdAsync()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddProcessExecution();
+            var sp = services.BuildServiceProvider();
+
+            var executor = sp.GetRequiredService<IProcessExecutor>();
+
+            var exitCode = await executor.ExecuteAsync(
+                new ProcessSpecification
+                {
+                    FilePath = @"C:\Windows\system32\cmd.exe",
+                    Arguments = new[]
+                    {
+                        "/C",
+                        "exit 1",
+                    },
+                },
+                CaptureSpecification.Passthrough,
+                CancellationToken.None);
+            Assert.Equal(1, exitCode);
+        }
+
+        [SkippableFact]
+        public async Task CanGetExitCodeWeirdFromCmdAsync()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddProcessExecution();
+            var sp = services.BuildServiceProvider();
+
+            var executor = sp.GetRequiredService<IProcessExecutor>();
+
+            var exitCode = await executor.ExecuteAsync(
+                new ProcessSpecification
+                {
+                    FilePath = @"C:\Windows\system32\cmd.exe",
+                    Arguments = new[]
+                    {
+                        "/C",
+                        "exit 375",
+                    },
+                },
+                CaptureSpecification.Passthrough,
+                CancellationToken.None);
+            Assert.Equal(375, exitCode);
+        }
+
+        [SkippableFact]
+        public async Task CanTerminateCmdWithTimeoutAsync()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddProcessExecution();
+            var sp = services.BuildServiceProvider();
+
+            var executor = sp.GetRequiredService<IProcessExecutor>();
+
+            var cts = new CancellationTokenSource(1000);
+            await Assert.ThrowsAsync<OperationCanceledException>(async () =>
+            {
+                await executor.ExecuteAsync(
+                    new ProcessSpecification
+                    {
+                        FilePath = @"C:\Windows\system32\cmd.exe",
+                        Arguments = new[]
+                        {
+                            "/C",
+                            "timeout 5 >NUL",
+                        },
+                    },
+                    CaptureSpecification.Passthrough,
+                    cts.Token);
+            });
+        }
+    }
+}
---
+++ UET/Redpoint.ProcessExecution/Windows/SystemCopy/PasteArguments.cs
@@ -0,0 +1,102 @@
+﻿namespace Redpoint.ProcessExecution.Windows.SystemCopy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <remarks>
+    /// From https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/PasteArguments.cs.
+    /// </remarks>
+    internal static partial class PasteArguments
+    {
+        internal static void AppendArgument(ref StringBuilder stringBuilder, string argument)
+        {
+            if (stringBuilder.Length != 0)
+            {
+                stringBuilder.Append(' ');
+            }
+
+            // Parsing rules for non-argv[0] arguments:
+            //   - Backslash is a normal character except followed by a quote.
+            //   - 2N backslashes followed by a quote ==> N literal backslashes followed by unescaped quote
+            //   - 2N+1 backslashes followed by a quote ==> N literal backslashes followed by a literal quote
+            //   - Parsing stops at first whitespace outside of quoted region.
+            //   - (post 2008 rule): A closing quote followed by another quote ==> literal quote, and parsing remains in quoting mode.
+            if (argument.Length != 0 && ContainsNoWhitespaceOrQuotes(argument))
+            {
+                // Simple case - no quoting or changes needed.
+                stringBuilder.Append(argument);
+            }
+            else
+            {
+                stringBuilder.Append(_quote);
+                int idx = 0;
+                while (idx < argument.Length)
+                {
+                    char c = argument[idx++];
+                    if (c == _backslash)
+                    {
+                        int numBackSlash = 1;
+                        while (idx < argument.Length && argument[idx] == _backslash)
+                        {
+                            idx++;
+                            numBackSlash++;
+                        }
+
+                        if (idx == argument.Length)
+                        {
+                            // We'll emit an end quote after this so must double the number of backslashes.
+                            stringBuilder.Append(_backslash, numBackSlash * 2);
+                        }
+                        else if (argument[idx] == _quote)
+                        {
+                            // Backslashes will be followed by a quote. Must double the number of backslashes.
+                            stringBuilder.Append(_backslash, numBackSlash * 2 + 1);
+                            stringBuilder.Append(_quote);
+                            idx++;
+                        }
+                        else
+                        {
+                            // Backslash will not be followed by a quote, so emit as normal characters.
+                            stringBuilder.Append(_backslash, numBackSlash);
+                        }
+
+                        continue;
+                    }
+
+                    if (c == _quote)
+                    {
+                        // Escape the quote so it appears as a literal. This also guarantees that we won't end up generating a closing quote followed
+                        // by another quote (which parses differently pre-2008 vs. post-2008.)
+                        stringBuilder.Append(_backslash);
+                        stringBuilder.Append(_quote);
+                        continue;
+                    }
+
+                    stringBuilder.Append(c);
+                }
+
+                stringBuilder.Append(_quote);
+            }
+        }
+
+        private static bool ContainsNoWhitespaceOrQuotes(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (char.IsWhiteSpace(c) || c == _quote)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private const char _quote = '\"';
+        private const char _backslash = '\\';
+    }
+}
