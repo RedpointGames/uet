b52312022c744592d2d93ceef62f6052fb0932d7
June Rhodes <jrhodes@redpoint.games>
2023-08-21 23:36:39 +1000

    Permit dashes in container registry hostnames

--- UET/Redpoint.Uefs.ContainerRegistry/RegistryTagRegex.cs
+++ UET/Redpoint.Uefs.ContainerRegistry/RegistryTagRegex.cs
@@ -10,6 +10,6 @@
         /// <summary>
         /// The regular expression used for parsing container images (like <c>host.com/path/subpath/image:tag</c>).
         /// </summary>
-        public static readonly Regex Regex = new Regex("^(?<host>[a-z\\.\\:0-9]+)/(?<path>[a-z\\./0-9-_]+):?(?<label>[a-z\\.0-9-_]+)?$");
+        public static readonly Regex Regex = new Regex(@"^(?<host>[a-z\.\:0-9-]+)/(?<path>[a-z\./0-9-_]+):?(?<label>[a-z\.0-9-_]+)?$");
     }
 }
