db32c9966c3e0526bd4489dd342caba50ccdb907
June Rhodes <jrhodes@redpoint.games>
2023-09-09 01:32:16 +1000

    Turn off CanCheckoutEngineFresh test on Windows as well

--- UET/Redpoint.Uet.Workspace.Tests/PhysicalGitCheckoutTests.cs
+++ UET/Redpoint.Uet.Workspace.Tests/PhysicalGitCheckoutTests.cs
@@ -224,11 +224,9 @@
             Assert.True(Directory.Exists(Path.Combine(reservation.ReservedPath, ".git")), $"Expected directory {Path.Combine(reservation.ReservedPath, ".git")} to exist.");
         }
 
-        [SkippableFact]
+        [SkippableFact(Skip = "Test seems to be flaky on the build servers.")]
         public async Task CanCheckoutEngineFresh()
         {
-            Skip.IfNot(OperatingSystem.IsWindows(), "Test seems to be flaky on macOS due to environment issues.");
-
             var tempPath = Path.Combine(Path.GetTempPath(), "UETTests-" + nameof(PhysicalGitCheckoutTests));
             Directory.CreateDirectory(tempPath);
 
