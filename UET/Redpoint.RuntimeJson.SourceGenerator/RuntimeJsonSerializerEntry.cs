55d9ed0d32c8b02906bd4f2b7b0b7f614136d448
June Rhodes <jrhodes@redpoint.games>
2023-09-15 18:13:33 +1000

    Implement commandlet-based tests for projects

--- UET/Redpoint.RuntimeJson.SourceGenerator/RuntimeJsonSerializerEntry.cs
+++ UET/Redpoint.RuntimeJson.SourceGenerator/RuntimeJsonSerializerEntry.cs
@@ -5,6 +5,6 @@
         public string? Namespace { get; set; }
         public string? Class { get; set; }
         public string? JsonSerializerContextType { get; set; }
-        public List<string> SerializableClassNames { get; } = new List<string>();
+        public HashSet<string> SerializableClassNames { get; } = new HashSet<string>();
     }
 }
--- UET/Redpoint.Uet.BuildPipeline.Providers.Test/TestProviderSourceGenerationContext.cs
+++ UET/Redpoint.Uet.BuildPipeline.Providers.Test/TestProviderSourceGenerationContext.cs
@@ -6,6 +6,7 @@
     using Redpoint.Uet.BuildPipeline.Providers.Test.Plugin.Downstream;
     using Redpoint.Uet.BuildPipeline.Providers.Test.Plugin.Gauntlet;
     using Redpoint.Uet.BuildPipeline.Providers.Test.Project.Automation;
+    using Redpoint.Uet.BuildPipeline.Providers.Test.Project.Commandlet;
     using Redpoint.Uet.BuildPipeline.Providers.Test.Project.Custom;
     using Redpoint.Uet.BuildPipeline.Providers.Test.Project.Gauntlet;
     using System.Text.Json;
@@ -20,6 +21,7 @@
     [JsonSerializable(typeof(BuildConfigProjectTestAutomation))]
     [JsonSerializable(typeof(BuildConfigProjectTestCustom))]
     [JsonSerializable(typeof(BuildConfigProjectTestGauntlet))]
+    [JsonSerializable(typeof(BuildConfigProjectTestCommandlet))]
     internal sealed partial class TestProviderSourceGenerationContext : JsonSerializerContext
     {
         public static TestProviderSourceGenerationContext WithStringEnum { get; } = new TestProviderSourceGenerationContext(new JsonSerializerOptions
