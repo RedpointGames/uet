a9e4d681478367d73c1eef40137bf08a79f09685
June Rhodes <jrhodes@redpoint.games>
2023-09-19 13:23:55 +1000

    Ensure WindowsGlobalMutexReservation can not be double disposed

--- UET/Redpoint.Reservation/WindowsGlobalMutexReservation.cs
+++ UET/Redpoint.Reservation/WindowsGlobalMutexReservation.cs
@@ -2,21 +2,31 @@
 {
     using System.Runtime.Versioning;
     using System.Threading.Tasks;
+    using Mutex = Redpoint.Concurrency.Mutex;
 
     [SupportedOSPlatform("windows")]
     internal sealed class WindowsGlobalMutexReservation : IGlobalMutexReservation
     {
         private readonly Semaphore _semaphore;
+        private readonly Mutex _semaphoreReleaseMutex;
+        private bool _disposed;
 
         public WindowsGlobalMutexReservation(Semaphore semaphore)
         {
             _semaphore = semaphore;
+            _semaphoreReleaseMutex = new Mutex();
         }
 
-        public ValueTask DisposeAsync()
+        public async ValueTask DisposeAsync()
         {
-            _semaphore.Release();
-            return ValueTask.CompletedTask;
+            using (await _semaphoreReleaseMutex.WaitAsync(CancellationToken.None).ConfigureAwait(false))
+            {
+                if (!_disposed)
+                {
+                    _semaphore.Release();
+                    _disposed = true;
+                }
+            }
         }
     }
 }
