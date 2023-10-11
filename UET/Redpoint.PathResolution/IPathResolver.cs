38fc712e090a7b38c5a917dbdea34b6d241fb211
June Rhodes <jrhodes@redpoint.games>
2023-10-11 14:23:00 +1100

    Publish Redpoint.PathResolution package

--- UET/Redpoint.PathResolution/IPathResolver.cs
+++ UET/Redpoint.PathResolution/IPathResolver.cs
@@ -1,7 +1,16 @@
 ﻿namespace Redpoint.PathResolution
 {
+    /// <summary>
+    /// Provides an interface which resolves executable binary names to absolute paths via the PATH environment variable.
+    /// </summary>
     public interface IPathResolver
     {
+        /// <summary>
+        /// Resolve the specified executable name to an absolute path if the executable binary exists on the current PATH.
+        /// </summary>
+        /// <param name="binaryName">The name of the executable binary, such as 'git'.</param>
+        /// <returns>The absolute path to the executable binary.</returns>
+        /// <exception cref="FileNotFoundException">Thrown if no such executable binary exists on the current PATH.</exception>
         Task<string> ResolveBinaryPath(string binaryName);
     }
 }
--- UET/Redpoint.PathResolution/PathResolutionServiceExtensions.cs
+++ UET/Redpoint.PathResolution/PathResolutionServiceExtensions.cs
@@ -6,8 +6,15 @@ namespace Redpoint.PathResolution
 {
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <summary>
+    /// Provides registration functions to register an implementation of <see cref="IPathResolver"/> into a <see cref="IServiceCollection"/>.
+    /// </summary>
     public static class PathResolutionServiceExtensions
     {
+        /// <summary>
+        /// Add path resolution services (the <see cref="IPathResolver"/> service) into the service collection.
+        /// </summary>
+        /// <param name="services">The service collection to register an implementation of <see cref="IPathResolver"/> to.</param>
         public static void AddPathResolution(this IServiceCollection services)
         {
             services.AddSingleton<IPathResolver, DefaultPathResolver>();
