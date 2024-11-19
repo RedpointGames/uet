3d80a092936b6eb34a2d89ca77614d97175edaf4
Jasper de Laat <jasper.de.laat@live.nl>
2024-11-19 23:16:29 +0100

    Implement custom deployment provider similar to custom test provider.

--- UET/Redpoint.Uet.BuildPipeline.Providers.Deployment/Project/Custom/BuildConfigProjectDeploymentCustom.cs
+++ UET/Redpoint.Uet.BuildPipeline.Providers.Deployment/Project/Custom/BuildConfigProjectDeploymentCustom.cs
@@ -9,11 +9,6 @@
         /// The script receives the following parameters:
         ///   -EnginePath "C:\Path\To\UnrealEngine"
         ///   -StageDirectory "C:\Path\To\UProject\Saved\StagedBuilds"
-        ///   -PackageDirectory "C:\Path\To\UProject\Saved\StagedBuilds\LinuxServer"
-        ///   -PackageType "Game"
-        ///   -PackageTarget "ExampleOSS"
-        ///   -PackagePlatform "Win64"
-        ///   -PackageConfiguration "DebugGame"
         /// </summary>
         [JsonPropertyName("ScriptPath"), JsonRequired]
         public string ScriptPath { get; set; } = string.Empty;
