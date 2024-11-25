9e049d31667daa36fb78ae0e6a355d6a42b41d5f
Jasper de Laat <jasper.de.laat@live.nl>
2024-11-25 04:05:40 +0100

    Implement custom deployment provider similar to custom test provider. (#55)

--- UET/Redpoint.Uet.BuildPipeline.Providers.Deployment/Project/Custom/CustomProjectDeploymentProvider.cs
+++ UET/Redpoint.Uet.BuildPipeline.Providers.Deployment/Project/Custom/CustomProjectDeploymentProvider.cs
@@ -1,6 +1,7 @@
 ﻿namespace Redpoint.Uet.BuildPipeline.Providers.Deployment.Project.Custom
 {
     using Redpoint.RuntimeJson;
+    using Redpoint.Uet.BuildGraph;
     using Redpoint.Uet.Configuration.Dynamic;
     using Redpoint.Uet.Configuration.Project;
     using System;
@@ -14,13 +15,51 @@
 
         public IRuntimeJson DynamicSettings { get; } = new DeploymentProviderRuntimeJson(DeploymentProviderSourceGenerationContext.WithStringEnum).BuildConfigProjectDeploymentCustom;
 
-        public Task WriteBuildGraphNodesAsync(
+        public async Task WriteBuildGraphNodesAsync(
             IBuildGraphEmitContext context,
             XmlWriter writer,
             BuildConfigProjectDistribution buildConfigDistribution,
             IEnumerable<BuildConfigDynamic<BuildConfigProjectDistribution, IDeploymentProvider>> elements)
         {
-            throw new NotImplementedException();
+            var castedSettings = elements
+                .Select(x => (name: x.Name, manual: x.Manual ?? false, settings: (BuildConfigProjectDeploymentCustom)x.DynamicSettings))
+                .ToList();
+
+            foreach (var deployment in castedSettings)
+            {
+                await writer.WriteAgentNodeAsync(
+                    new AgentNodeElementProperties
+                    {
+                        AgentStage = $"Deployment {deployment.name}",
+                        AgentType = deployment.manual ? "Win64_Manual" : "Win64",
+                        NodeName = $"Deployment {deployment.name}",
+                        Requires = $"$(GameStaged);$(ClientStaged);$(ServerStaged);$(DynamicPreDeploymentNodes)",
+                    },
+                    async writer =>
+                    {
+                        await writer.WriteSpawnAsync(
+                            new SpawnElementProperties
+                            {
+                                Exe = "powershell.exe",
+                                Arguments = new[]
+                                {
+                                    "-ExecutionPolicy",
+                                    "Bypass",
+                                    "-File",
+                                    $@"""$(RepositoryRoot)/{deployment.settings.ScriptPath}""",
+                                    "-EnginePath",
+                                    $@"""$(EnginePath)""",
+                                    "-StageDirectory",
+                                    $@"""$(StageDirectory)""",
+                                }
+                            }).ConfigureAwait(false);
+                    }).ConfigureAwait(false);
+                await writer.WriteDynamicNodeAppendAsync(
+                    new DynamicNodeAppendElementProperties
+                    {
+                        NodeName = $"Deployment {deployment.name}",
+                    }).ConfigureAwait(false);
+            }
         }
     }
 }
