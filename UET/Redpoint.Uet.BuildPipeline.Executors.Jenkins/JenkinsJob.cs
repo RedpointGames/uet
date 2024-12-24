822bd6b03a33e05734cbe6d07fa669ce0dfeab36
Jasper de Laat <jasper.de.laat@live.nl>
2024-12-24 23:46:56 +0100

    WIP: Initial implementation of Jenkins (controller) executor.

---
+++ UET/Redpoint.Uet.BuildPipeline.Executors.Jenkins/JenkinsJob.cs
@@ -0,0 +1,13 @@
+﻿namespace Redpoint.Uet.BuildPipeline.Executors.Jenkins
+{
+    public class JenkinsJob
+    {
+        public JenkinsJobStatus Status { get; set; } = JenkinsJobStatus.Idle;
+
+        public Uri? QueueUri { get; set; }
+
+        public Uri? ExecutionUri { get; set; }
+
+        public int ExecutionLogByteOffset { get; set; }
+    }
+}
