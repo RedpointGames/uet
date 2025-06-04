3c746d6a3971b85ba1ae1d1b60bb411445b67bc1
June Rhodes <jrhodes@redpoint.games>
2025-06-04 17:18:42 +1000

    Fix RZFS dataset and delete obsolete datasets (#260)

---
+++ UET/uet/Commands/Internal/RemoteZfsServer/TrueNasDataset.cs
@@ -0,0 +1,19 @@
+﻿namespace UET.Commands.Internal.RemoteZfsServer
+{
+    using System.Text.Json.Serialization;
+
+    internal class TrueNasDataset
+    {
+        [JsonPropertyName("pool")]
+        public required string Pool { get; set; }
+
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("id")]
+        public required string Id { get; set; }
+
+        [JsonPropertyName("children")]
+        public required TrueNasDataset[] Children { get; set; }
+    }
+}
