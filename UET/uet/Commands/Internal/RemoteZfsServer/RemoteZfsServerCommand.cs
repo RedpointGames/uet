9af473276c6872e7744a3a737a2e579555394535
June Rhodes <jrhodes@redpoint.games>
2025-06-16 05:38:07 +1000

    Log exceptions during connection close for RZFS (#266)

--- UET/uet/Commands/Internal/RemoteZfsServer/RemoteZfsServerCommand.cs
+++ UET/uet/Commands/Internal/RemoteZfsServer/RemoteZfsServerCommand.cs
@@ -222,127 +222,134 @@
                     throw new InvalidOperationException();
                 }
 
-                _logger.LogInformation($"Obtained workspace request for template {request.TemplateId}.");
-
-                var template = _config.Templates.FirstOrDefault(x => x.TemplateId == request.TemplateId);
-                if (template == null)
-                {
-                    throw new RpcException(new Status(StatusCode.NotFound, "no such template exists."));
-                }
-
-                _logger.LogInformation($"Looking for latest ZFS snapshot for dataset '{template.ZfsSnapshotDataset}'.");
-
-                using var httpClient = new HttpClient(new HttpClientHandler
-                {
-                    // Ignore TLS errors because TrueNAS is usually running with a self-signed certificate on the local network.
-                    ClientCertificateOptions = ClientCertificateOption.Manual,
-                    ServerCertificateCustomValidationCallback = (_, _, _, _) => true,
-                });
-                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + _config.TrueNasApiKey);
-
-                var latestSnapshotResponse = await httpClient.SendAsync(
-                    new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Get,
-                        Content = new StringContent(
-                            JsonSerializer.Serialize(
-                                new TrueNasQuery
-                                {
-                                    QueryFilters = new[]
-                                    {
-                                        new[] { "dataset", "=", template.ZfsSnapshotDataset },
-                                    },
-                                    QueryOptions = new TrueNasQueryOptions
-                                    {
-                                        Extra = new Dictionary<string, string> { { "properties", "name, createtxg" } },
-                                        OrderBy = new[] { "-createtxg" },
-                                        Limit = 1,
-                                    }
-                                },
-                                RemoteZfsSerializerContext.Default.TrueNasQuery),
-                            new System.Net.Http.Headers.MediaTypeHeaderValue("application/json")),
-                        RequestUri = new Uri($"{_config.TrueNasUrl}/zfs/snapshot"),
-                    },
-                    cancellationToken: context.CancellationToken).ConfigureAwait(false);
-                latestSnapshotResponse.EnsureSuccessStatusCode();
-
-                var latestSnapshot = await latestSnapshotResponse.Content
-                    .ReadFromJsonAsync(RemoteZfsSerializerContext.Default.TrueNasSnapshotArray)
-                    .ConfigureAwait(false);
-
-                if (latestSnapshot == null || latestSnapshot.Length != 1)
-                {
-                    throw new RpcException(new Status(StatusCode.NotFound, "the target dataset has no latest snapshot."));
-                }
-
-                var rzfsTimestamp = $"RZFS_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
-                var rzfsId = $"{template.LinuxParentDataset}/{rzfsTimestamp}";
-
-                _logger.LogInformation($"Cloning ZFS snapshot to dataset '{rzfsId}'.");
-
-                _usedDatasets.TryAdd(rzfsId, true);
                 try
                 {
-                    var cloneResponse = await httpClient.SendAsync(
+                    _logger.LogInformation($"Obtained workspace request for template {request.TemplateId}.");
+
+                    var template = _config.Templates.FirstOrDefault(x => x.TemplateId == request.TemplateId);
+                    if (template == null)
+                    {
+                        throw new RpcException(new Status(StatusCode.NotFound, "no such template exists."));
+                    }
+
+                    _logger.LogInformation($"Looking for latest ZFS snapshot for dataset '{template.ZfsSnapshotDataset}'.");
+
+                    using var httpClient = new HttpClient(new HttpClientHandler
+                    {
+                        // Ignore TLS errors because TrueNAS is usually running with a self-signed certificate on the local network.
+                        ClientCertificateOptions = ClientCertificateOption.Manual,
+                        ServerCertificateCustomValidationCallback = (_, _, _, _) => true,
+                    });
+                    httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + _config.TrueNasApiKey);
+
+                    var latestSnapshotResponse = await httpClient.SendAsync(
                         new HttpRequestMessage
                         {
-                            Method = HttpMethod.Post,
+                            Method = HttpMethod.Get,
                             Content = new StringContent(
                                 JsonSerializer.Serialize(
-                                    new TrueNasSnapshotClone
+                                    new TrueNasQuery
                                     {
-                                        Snapshot = latestSnapshot[0].Id,
-                                        DatasetDest = rzfsId,
+                                        QueryFilters = new[]
+                                        {
+                                            new[] { "dataset", "=", template.ZfsSnapshotDataset },
+                                        },
+                                        QueryOptions = new TrueNasQueryOptions
+                                        {
+                                            Extra = new Dictionary<string, string> { { "properties", "name, createtxg" } },
+                                            OrderBy = new[] { "-createtxg" },
+                                            Limit = 1,
+                                        }
                                     },
-                                    RemoteZfsSerializerContext.Default.TrueNasSnapshotClone),
+                                    RemoteZfsSerializerContext.Default.TrueNasQuery),
                                 new System.Net.Http.Headers.MediaTypeHeaderValue("application/json")),
-                            RequestUri = new Uri($"{_config.TrueNasUrl}/zfs/snapshot/clone"),
+                            RequestUri = new Uri($"{_config.TrueNasUrl}/zfs/snapshot"),
                         },
-                        cancellationToken: CancellationToken.None /* so this operation can't be interrupted after it succeeds */).ConfigureAwait(false);
-                    cloneResponse.EnsureSuccessStatusCode();
+                        cancellationToken: context.CancellationToken).ConfigureAwait(false);
+                    latestSnapshotResponse.EnsureSuccessStatusCode();
 
+                    var latestSnapshot = await latestSnapshotResponse.Content
+                        .ReadFromJsonAsync(RemoteZfsSerializerContext.Default.TrueNasSnapshotArray)
+                        .ConfigureAwait(false);
+
+                    if (latestSnapshot == null || latestSnapshot.Length != 1)
+                    {
+                        throw new RpcException(new Status(StatusCode.NotFound, "the target dataset has no latest snapshot."));
+                    }
+
+                    var rzfsTimestamp = $"RZFS_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+                    var rzfsId = $"{template.LinuxParentDataset}/{rzfsTimestamp}";
+
+                    _logger.LogInformation($"Cloning ZFS snapshot to dataset '{rzfsId}'.");
+
+                    _usedDatasets.TryAdd(rzfsId, true);
                     try
                     {
-                        await responseStream.WriteAsync(new AcquireWorkspaceResponse
-                        {
-                            WindowsShareRemotePath = $"{template.WindowsNetworkShareParentPath}\\{rzfsTimestamp}",
-                        }, context.CancellationToken).ConfigureAwait(false);
+                        var cloneResponse = await httpClient.SendAsync(
+                            new HttpRequestMessage
+                            {
+                                Method = HttpMethod.Post,
+                                Content = new StringContent(
+                                    JsonSerializer.Serialize(
+                                        new TrueNasSnapshotClone
+                                        {
+                                            Snapshot = latestSnapshot[0].Id,
+                                            DatasetDest = rzfsId,
+                                        },
+                                        RemoteZfsSerializerContext.Default.TrueNasSnapshotClone),
+                                    new System.Net.Http.Headers.MediaTypeHeaderValue("application/json")),
+                                RequestUri = new Uri($"{_config.TrueNasUrl}/zfs/snapshot/clone"),
+                            },
+                            cancellationToken: CancellationToken.None /* so this operation can't be interrupted after it succeeds */).ConfigureAwait(false);
+                        cloneResponse.EnsureSuccessStatusCode();
 
-                        _logger.LogInformation($"Waiting for client to close connection...");
-                        while (!context.CancellationToken.IsCancellationRequested)
+                        try
                         {
-                            await Task.Delay(1000, context.CancellationToken);
+                            await responseStream.WriteAsync(new AcquireWorkspaceResponse
+                            {
+                                WindowsShareRemotePath = $"{template.WindowsNetworkShareParentPath}\\{rzfsTimestamp}",
+                            }, context.CancellationToken).ConfigureAwait(false);
+
+                            _logger.LogInformation($"Waiting for client to close connection...");
+                            while (!context.CancellationToken.IsCancellationRequested)
+                            {
+                                await Task.Delay(1000, context.CancellationToken);
+                            }
                         }
-                    }
-                    catch (OperationCanceledException)
-                    {
-                    }
-
-                    _logger.LogInformation($"Deleting ZFS dataset '{rzfsId}'...");
-
-                    var deleteResponse = await httpClient.SendAsync(
-                        new HttpRequestMessage
+                        catch (OperationCanceledException)
                         {
-                            Method = HttpMethod.Delete,
-                            Content = new StringContent(
-                                JsonSerializer.Serialize(
-                                    new TrueNasDeleteOptions
-                                    {
-                                        Recursive = true,
-                                        Force = true,
-                                    },
-                                    RemoteZfsSerializerContext.Default.TrueNasDeleteOptions),
-                                new System.Net.Http.Headers.MediaTypeHeaderValue("application/json")),
-                            RequestUri = new Uri($"{_config.TrueNasUrl}/pool/dataset/id/{HttpUtility.UrlEncode(rzfsId)}"),
-                        },
-                        cancellationToken: CancellationToken.None).ConfigureAwait(false);
-                    deleteResponse.EnsureSuccessStatusCode();
+                        }
 
-                    _logger.LogInformation($"ZFS request complete.");
+                        _logger.LogInformation($"Deleting ZFS dataset '{rzfsId}'...");
+
+                        var deleteResponse = await httpClient.SendAsync(
+                            new HttpRequestMessage
+                            {
+                                Method = HttpMethod.Delete,
+                                Content = new StringContent(
+                                    JsonSerializer.Serialize(
+                                        new TrueNasDeleteOptions
+                                        {
+                                            Recursive = true,
+                                            Force = true,
+                                        },
+                                        RemoteZfsSerializerContext.Default.TrueNasDeleteOptions),
+                                    new System.Net.Http.Headers.MediaTypeHeaderValue("application/json")),
+                                RequestUri = new Uri($"{_config.TrueNasUrl}/pool/dataset/id/{HttpUtility.UrlEncode(rzfsId)}"),
+                            },
+                            cancellationToken: CancellationToken.None).ConfigureAwait(false);
+                        deleteResponse.EnsureSuccessStatusCode();
+
+                        _logger.LogInformation($"ZFS request complete.");
+                    }
+                    finally
+                    {
+                        _usedDatasets.Remove(rzfsId, out _);
+                    }
                 }
-                finally
+                catch (Exception ex) when (context.CancellationToken.IsCancellationRequested && !(ex is OperationCanceledException))
                 {
-                    _usedDatasets.Remove(rzfsId, out _);
+                    _logger.LogError(ex, $"Exception while connection is already closed: {ex.Message}");
                 }
             }
         }
