dd950dd46513954d3537409b5862120d831202a5
June Rhodes <jrhodes@redpoint.games>
2025-07-06 19:39:49 +1000

    Update CloudFile class to work with System.Text.Json

---
+++ UET/Redpoint.CloudFramework/Storage/CloudFile.cs
@@ -0,0 +1,17 @@
+﻿namespace Redpoint.CloudFramework.Storage
+{
+    using Newtonsoft.Json;
+    using System.Text.Json.Serialization;
+
+    public class CloudFile
+    {
+        [JsonProperty("fileId"), JsonPropertyName("fileId")]
+        public string FileId { get; set; } = string.Empty;
+
+        [JsonProperty("filename"), JsonPropertyName("filename")]
+        public string Filename { get; set; } = string.Empty;
+
+        [JsonProperty("size"), JsonPropertyName("size")]
+        public long Size { get; internal set; }
+    }
+}
--- UET/Redpoint.CloudFramework/Storage/IFileStorage.cs
+++ UET/Redpoint.CloudFramework/Storage/IFileStorage.cs
@@ -1,47 +1,8 @@
 ﻿namespace Redpoint.CloudFramework.Storage
 {
-    using Newtonsoft.Json;
     using System.Collections.Generic;
     using System.IO;
     using System.Threading.Tasks;
-
-    public class CloudFile
-    {
-        [JsonProperty("fileId")]
-        public string FileId { get; set; } = string.Empty;
-
-        [JsonProperty("filename")]
-        public string Filename { get; set; } = string.Empty;
-
-        [JsonProperty("size")]
-        public long Size { get; internal set; }
-    }
-
-    public class CloudFileWithData : CloudFile
-    {
-        public CloudFileWithData(CloudFile file, byte[] data)
-        {
-            ArgumentNullException.ThrowIfNull(file);
-
-            FileId = file.FileId;
-            Filename = file.Filename;
-            FileData = data;
-        }
-
-        public ReadOnlyMemory<byte> FileData { get; set; }
-    }
-
-    public class FileStorageProfile
-    {
-        public FileStorageProfile(string name)
-        {
-            Name = name;
-        }
-
-        public string Name { get; }
-
-        public static FileStorageProfile Default { get; } = new FileStorageProfile("Default");
-    }
 
     public interface IFileStorage
     {
