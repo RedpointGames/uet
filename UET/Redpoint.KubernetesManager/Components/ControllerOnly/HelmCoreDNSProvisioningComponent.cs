3293a7eeb5eaa3151b2da405b3d75337974377ce
June Rhodes <jrhodes@redpoint.games>
2025-07-27 20:27:20 +1000

    Unify Helm deployment logic (#298)

---
+++ UET/Redpoint.KubernetesManager/Components/ControllerOnly/HelmCoreDNSProvisioningComponent.cs
@@ -0,0 +1,92 @@
+﻿namespace Redpoint.KubernetesManager.Components.ControllerOnly
+{
+    using Microsoft.Extensions.Logging;
+    using Redpoint.KubernetesManager.Models;
+    using Redpoint.KubernetesManager.Services;
+    using Redpoint.KubernetesManager.Services.Helm;
+    using Redpoint.KubernetesManager.Signalling;
+    using Redpoint.KubernetesManager.Signalling.Data;
+    using Redpoint.ProcessExecution;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The CoreDNS provisioning component installs or upgrades CoreDNS in the
+    /// cluster. It waits for the API server to be available so that it can then run Helm.
+    /// 
+    /// This component only runs on the controller.
+    /// </summary>
+    internal class HelmCoreDNSProvisioningComponent : IComponent
+    {
+        private readonly ILogger<HelmCoreDNSProvisioningComponent> _logger;
+        private readonly IClusterNetworkingConfiguration _clusterNetworkingConfiguration;
+        private readonly IHelmDeployment _helmDeployment;
+
+        public HelmCoreDNSProvisioningComponent(
+            ILogger<HelmCoreDNSProvisioningComponent> logger,
+            IClusterNetworkingConfiguration clusterNetworkingConfiguration,
+            IHelmDeployment helmDeployment)
+        {
+            _logger = logger;
+            _clusterNetworkingConfiguration = clusterNetworkingConfiguration;
+            _helmDeployment = helmDeployment;
+        }
+
+        public void RegisterSignals(IRegistrationContext context)
+        {
+            if (context.Role == RoleType.Controller)
+            {
+                context.OnSignal(WellKnownSignals.Started, OnStartedAsync);
+            }
+        }
+
+        private async Task OnStartedAsync(IContext context, IAssociatedData? data, CancellationToken cancellationToken)
+        {
+            // Deploy CoreDNS for Linux inside the cluster.
+            var exitCode = await _helmDeployment.DeployChart(
+                context,
+                "coredns",
+                "oci://ghcr.io/coredns/charts/coredns",
+                $"""
+                service:
+                  clusterIP: "{_clusterNetworkingConfiguration.ClusterDNSServiceIP}"
+                nodeSelector:
+                  kubernetes.io/os: linux
+                servers:
+                - zones:
+                  - zone: .
+                  port: 53
+                  plugins:
+                  - name: errors
+                  - name: health
+                    configBlock: |-
+                      lameduck 10s
+                  - name: ready
+                  - name: kubernetes
+                    parameters: {_clusterNetworkingConfiguration.ClusterDNSDomain} in-addr.arpa ip6.arpa
+                    configBlock: |-
+                      pods insecure
+                      fallthrough in-addr.arpa ip6.arpa
+                      ttl 30
+                  - name: prometheus
+                    parameters: 0.0.0.0:9153
+                  - name: forward
+                    parameters: . 1.1.1.1 1.0.0.1
+                  - name: cache
+                    parameters: 30
+                  - name: loop
+                  - name: reload
+                  - name: loadbalance
+                """,
+                cancellationToken);
+            if (exitCode != 0)
+            {
+                _logger.LogCritical("rkm is exiting because it could not deploy CoreDNS via Helm, and CoreDNS is required for networking to work.");
+                context.StopOnCriticalError();
+                return;
+            }
+
+            // CoreDNS has now been provisioned via Helm.
+            context.SetFlag(WellKnownFlags.CoreDNSProvisioned);
+        }
+    }
+}
---
+++ UET/Redpoint.KubernetesManager/Services/Helm/DefaultHelmDeployment.cs
@@ -0,0 +1,75 @@
+﻿namespace Redpoint.KubernetesManager.Services.Helm
+{
+    using Redpoint.KubernetesManager.Signalling;
+    using Redpoint.KubernetesManager.Signalling.Data;
+    using Redpoint.ProcessExecution;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    internal class DefaultHelmDeployment : IHelmDeployment
+    {
+        private readonly IPathProvider _pathProvider;
+        private readonly IKubeConfigManager _kubeConfigManager;
+        private readonly IProcessExecutor _processExecutor;
+
+        public DefaultHelmDeployment(
+            IPathProvider pathProvider,
+            IKubeConfigManager kubeConfigManager,
+            IProcessExecutor processExecutor)
+        {
+            _pathProvider = pathProvider;
+            _kubeConfigManager = kubeConfigManager;
+            _processExecutor = processExecutor;
+        }
+
+        public async Task<int> DeployChart(
+            IContext context,
+            string chartName,
+            string ociUrl,
+            string valuesContent,
+            CancellationToken cancellationToken)
+        {
+            await context.WaitForFlagAsync(WellKnownFlags.KubeConfigsReady);
+
+            // Wait for the Kubernetes API server to be available.
+            var kubernetesContext = await context.WaitForFlagAsync<KubernetesClientContextData>(WellKnownFlags.KubeApiServerReady);
+            var kubernetes = kubernetesContext.Kubernetes;
+
+            // The path to Helm that we extracted earlier.
+            var helmPath = Path.Combine(_pathProvider.RKMRoot, "helm-bin", "helm");
+
+            // Generate the values.yaml file for our deployment, since we can't reliably set plugin parameters
+            // via --set on the command line.
+            Directory.CreateDirectory(Path.Combine(_pathProvider.RKMRoot, "helm-values"));
+            var valuesPath = Path.Combine(_pathProvider.RKMRoot, "helm-values", $"{chartName}.yaml");
+            await File.WriteAllTextAsync(
+                valuesPath,
+                valuesContent,
+                cancellationToken);
+
+            // Install/upgrade via OCI charts.
+            var arguments = new List<LogicalProcessArgument>()
+            {
+                $"--kubeconfig={_kubeConfigManager.GetKubeconfigPath("users", "user-admin")}",
+                "--namespace=kube-system",
+                "upgrade",
+                "--install",
+                chartName,
+                ociUrl,
+                "--wait",
+                "--values",
+                valuesPath
+            };
+            var exitCode = await _processExecutor.ExecuteAsync(
+                new ProcessExecution.ProcessSpecification
+                {
+                    FilePath = helmPath,
+                    Arguments = arguments,
+                    WorkingDirectory = Path.GetDirectoryName(helmPath)!,
+                },
+                CaptureSpecification.Passthrough,
+                cancellationToken);
+            return exitCode;
+        }
+    }
+}
