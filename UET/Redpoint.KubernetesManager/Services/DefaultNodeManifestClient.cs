2a8d41dc3b39bdadaf5cb1417c48323f1b92c9c2
June Rhodes <jrhodes@redpoint.games>
2025-07-28 20:57:55 +1000

    Add more logging for manifest download in RKM node logic

--- UET/Redpoint.KubernetesManager/Services/DefaultNodeManifestClient.cs
+++ UET/Redpoint.KubernetesManager/Services/DefaultNodeManifestClient.cs
@@ -6,15 +6,20 @@
     using YamlDotNet.Serialization;
     using System.Net;
     using System.Web;
+    using Microsoft.Extensions.Logging;
 
     internal class DefaultNodeManifestClient : INodeManifestClient, IDisposable
     {
         private readonly IPathProvider _pathProvider;
+        private readonly ILogger<DefaultNodeManifestClient> _logger;
         private readonly SemaphoreSlim _semaphore;
 
-        public DefaultNodeManifestClient(IPathProvider pathProvider)
+        public DefaultNodeManifestClient(
+            IPathProvider pathProvider,
+            ILogger<DefaultNodeManifestClient> logger)
         {
             _pathProvider = pathProvider;
+            _logger = logger;
             _semaphore = new SemaphoreSlim(1);
         }
 
@@ -41,11 +46,17 @@
 
                 using (var client = new HttpClient())
                 {
+                    _logger.LogInformation($"Fetching manifest from: http://{controllerAddress}:8374/manifest");
                     var manifest = await client.GetStringAsync(new Uri($"http://{controllerAddress}:8374/manifest?nodeName=" + HttpUtility.UrlEncode(nodeName)), stoppingToken);
                     var manifestDeserialized = deserializer.Deserialize<NodeManifest>(manifest);
                     await File.WriteAllTextAsync(manifestPath, manifest, stoppingToken);
                     return manifestDeserialized;
                 }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Failed to download manifest: {ex.Message}");
+                throw;
             }
             finally
             {
