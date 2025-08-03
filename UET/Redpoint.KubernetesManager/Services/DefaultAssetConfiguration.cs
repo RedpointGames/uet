6694b4b6ab3f8c2be3402954ec5447be04f80263
June Rhodes <jrhodes@redpoint.games>
2025-08-04 01:28:18 +1000

    Remove downloading containerd in main RKM process (#305)
    The 'run-containerd' command now downloads containerd and runc as needed.

--- UET/Redpoint.KubernetesManager/Services/DefaultAssetConfiguration.cs
+++ UET/Redpoint.KubernetesManager/Services/DefaultAssetConfiguration.cs
@@ -9,8 +9,6 @@ namespace Redpoint.KubernetesManager.Services
     internal class DefaultAssetConfiguration : IAssetConfiguration
     {
         private const string _kubernetesVersion = "1.26.1";
-        private const string _containerdVersion = "1.6.18";
-        private const string _runcVersion = "1.3.0";
         private const string _etcdVersion = "3.6.4";
         private const string _cniPluginsVersion = "0.8.7";
         private const string _helmVersion = "3.17.3";
@@ -20,12 +18,6 @@ namespace Redpoint.KubernetesManager.Services
             { "KubernetesNode:Linux", $"https://dl.k8s.io/v{_kubernetesVersion}/kubernetes-node-linux-amd64.tar.gz" },
             { "KubernetesNode:Windows", $"https://dl.k8s.io/v{_kubernetesVersion}/kubernetes-node-windows-amd64.tar.gz" },
             { "KubernetesServer:Linux", $"https://dl.k8s.io/v{_kubernetesVersion}/kubernetes-server-linux-amd64.tar.gz" },
-            { "Containerd:Linux", $"https://github.com/containerd/containerd/releases/download/v{_containerdVersion}/containerd-{_containerdVersion}-linux-amd64.tar.gz" },
-            { "Containerd:Windows", $"https://github.com/containerd/containerd/releases/download/v{_containerdVersion}/containerd-{_containerdVersion}-windows-amd64.tar.gz" },
-            // A temporary patched version of containerd that works on Windows 11 until the following pull request is
-            // in a containerd release: https://github.com/containerd/containerd/pull/8137
-            { "ContainerdForWin11:Windows", $"https://dl-public.redpoint.games/file/dl-public-redpoint-games/redpoint-containerd-for-win11-{_containerdVersion}.zip" },
-            { "Runc:Linux", $"https://github.com/opencontainers/runc/releases/download/v{_runcVersion}/runc.amd64" },
             { "Etcd:Linux", $"https://github.com/etcd-io/etcd/releases/download/v{_etcdVersion}/etcd-v{_etcdVersion}-linux-amd64.tar.gz" },
             { "CniPlugins:Linux", $"https://github.com/containernetworking/plugins/releases/download/v{_cniPluginsVersion}/cni-plugins-linux-amd64-v{_cniPluginsVersion}.tgz" },
             { "UbuntuWSL:Windows", $"https://wslstorestorage.blob.core.windows.net/wslblob/CanonicalGroupLimited.UbuntuonWindows_2004.2021.825.0.AppxBundle" },
