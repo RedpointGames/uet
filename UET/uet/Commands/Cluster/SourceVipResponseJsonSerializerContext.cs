f6b225309a50b844c3c9f0464c9e05b2e4aa8047
June Rhodes <jrhodes@redpoint.games>
2025-08-07 16:16:47 +1000

    Add command to allocate source VIP for kube-proxy (#321)

---
+++ UET/uet/Commands/Cluster/SourceVipResponseJsonSerializerContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UET.Commands.Cluster
+{
+    [JsonSerializable(typeof(SourceVipResponse))]
+    public partial class SourceVipResponseJsonSerializerContext : JsonSerializerContext
+    {
+    }
+}
