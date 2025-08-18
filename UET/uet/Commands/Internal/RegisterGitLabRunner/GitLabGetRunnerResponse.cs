c798ababdf72b8f2bf75b6017a84d3f22113472b
June Rhodes <jrhodes@redpoint.games>
2025-08-19 00:03:15 +1000

    Add internal command to register GitLab runners
    This makes it easier to register GitLab runners with the new authentication flow.

---
+++ UET/uet/Commands/Internal/RegisterGitLabRunner/GitLabGetRunnerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace UET.Commands.Internal.RegisterGitLabRunner
+{
+    using System.Text.Json.Serialization;
+
+    internal class GitLabGetRunnerResponse
+    {
+        [JsonPropertyName("id"), JsonRequired]
+        public required int Id { get; set; }
+    }
+}
---
+++ UET/uet/Commands/Internal/RegisterGitLabRunner/RegisterGitLabRunnerJsonSerializerContext.cs
@@ -0,0 +1,11 @@
+﻿namespace UET.Commands.Internal.RegisterGitLabRunner
+{
+    using System.Text.Json.Serialization;
+
+    [JsonSerializable(typeof(GitLabRunnerRegistrationSpec[]))]
+    [JsonSerializable(typeof(GitLabGetRunnerResponse))]
+    [JsonSerializable(typeof(GitLabRegisterRunnerResponse))]
+    internal partial class RegisterGitLabRunnerJsonSerializerContext : JsonSerializerContext
+    {
+    }
+}
