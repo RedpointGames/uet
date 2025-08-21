b7f3c2a18452e6ac3e36c726015b1ea7f6f23036
June Rhodes <jrhodes@redpoint.games>
2025-08-22 00:34:44 +1000

    Prevent Git from asking for credentials on terminal

--- UET/Redpoint.Uet.Workspace/PhysicalGit/DefaultPhysicalGitCheckout.cs
+++ UET/Redpoint.Uet.Workspace/PhysicalGit/DefaultPhysicalGitCheckout.cs
@@ -1970,6 +1970,7 @@
                 var envVars = new Dictionary<string, string>
                 {
                     { "GIT_ASK_YESNO", "false" },
+                    { "GIT_TERMINAL_PROMPT", "false" },
                     { "GIT_CONFIG_COUNT", "1" },
                     { "GIT_CONFIG_KEY_0", "credential.helper" },
                     { "GIT_CONFIG_VALUE_0", $"{_gitCredentialHelperProvider.FilePath.Replace(@"\", @"\\", StringComparison.Ordinal)} {string.Join(' ', _gitCredentialHelperProvider.Arguments.Select(x => x.LogicalValue))}" },
