79d84e69cd56d502a5ca1742400ee070a8ba94e1
June Rhodes <jrhodes@redpoint.games>
2025-11-24 11:30:05 +1100

    Retry 'git lfs fetch' on network error

---
+++ UET/Redpoint.Uet.Workspace/PhysicalGit/GitLfsCaptureSpecification.cs
@@ -0,0 +1,66 @@
+﻿namespace Redpoint.Uet.Workspace.PhysicalGit
+{
+    using Redpoint.ProcessExecution;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GitLfsCaptureSpecification : ICaptureSpecification
+    {
+        private readonly ICaptureSpecification _proxied;
+
+        public GitLfsCaptureSpecification(ICaptureSpecification proxied)
+        {
+            _proxied = proxied;
+        }
+
+        public bool NeedsRetry { get; set; }
+
+        public bool InterceptStandardInput => _proxied.InterceptStandardInput;
+
+        public bool InterceptStandardOutput => true;
+
+        public bool InterceptStandardError => _proxied.InterceptStandardError;
+
+        private static bool ShouldRetry(string data)
+        {
+            if (data.Contains($"batch response: Fatal error: Server error:", StringComparison.Ordinal))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void OnReceiveStandardError(string data)
+        {
+            ArgumentNullException.ThrowIfNull(data);
+
+            if (ShouldRetry(data))
+            {
+                NeedsRetry = true;
+            }
+
+            _proxied.OnReceiveStandardError(data);
+        }
+
+        public void OnReceiveStandardOutput(string data)
+        {
+            ArgumentNullException.ThrowIfNull(data);
+
+            if (ShouldRetry(data))
+            {
+                NeedsRetry = true;
+            }
+
+            _proxied.OnReceiveStandardOutput(data);
+        }
+
+        public string? OnRequestStandardInputAtStartup()
+        {
+            return _proxied.OnRequestStandardInputAtStartup();
+        }
+    }
+}
---
+++ UET/Redpoint.Uet.Workspace/PhysicalGit/GitLfsFileNotCheckedOutProperlyException.cs
@@ -0,0 +1,12 @@
+﻿namespace Redpoint.Uet.Workspace.PhysicalGit
+{
+    using System;
+
+    public class GitLfsFileNotCheckedOutProperlyException : Exception
+    {
+        public GitLfsFileNotCheckedOutProperlyException(string fullName)
+            : base($"Found Git LFS pointer in file: {fullName}")
+        {
+        }
+    }
+}
