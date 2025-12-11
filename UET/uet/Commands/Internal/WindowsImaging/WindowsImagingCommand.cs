e59c48cdf261b8cc1f2c2d155c9796a5d2c6e5bb
June Rhodes <jrhodes@redpoint.games>
2025-12-11 23:05:15 +1100

    Add experimental command for creating PXEboot into UET on WinPE (#419)

---
+++ UET/uet/Commands/Internal/WindowsImaging/WindowsImagingCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace UET.Commands.Internal.WindowsImaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.CommandLine;
+    using System.Linq;
+    using System.Text;
+    using UET.Commands.Cluster;
+    using UET.Commands.Internal.RunRemote;
+
+    internal class WindowsImagingCommand
+    {
+        public static Command CreateWindowsImagingCommand()
+        {
+            var command = new Command("windows-imaging", "Create various boot images for Windows clients.");
+            command.AddCommand(WindowsImagingCreatePxeBootCommand.CreateWindowsImagingCreatePxeBootCommand());
+            return command;
+        }
+    }
+}
