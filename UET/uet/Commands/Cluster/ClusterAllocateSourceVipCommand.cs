ef709b56f1d95236162e362c3e370870dd8c34eb
June Rhodes <jrhodes@redpoint.games>
2025-12-17 18:20:37 +1100

    Add liveness probe for allocate-source-vip command (#428)
    This is an attempt to mitigate the init container stalling forever when provisioned via PXE Boot.

--- UET/uet/Commands/Cluster/ClusterAllocateSourceVipCommand.cs
+++ UET/uet/Commands/Cluster/ClusterAllocateSourceVipCommand.cs
@@ -6,6 +6,8 @@ using Redpoint.ServiceControl;
 using Redpoint.Windows.HostNetworkingService;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Text;
@@ -34,6 +36,7 @@ namespace UET.Commands.Cluster
             {
                 IsRequired = true,
             };
+            public Option<int?> LivenessProbePort = new Option<int?>("--liveness-probe-port", "An optional port that the 'allocate-source-vip' command should listen on for localhost while it is performing work. This can be set as the liveness probe for the init container to detect if the allocate source VIP command is not running properly.");
         }
 
         public static Command CreateClusterAllocateSourceVipCommand()
@@ -80,133 +83,195 @@ namespace UET.Commands.Cluster
                 var cniBinPath = context.ParseResult.GetValueForOption(_options.CniBinPath)!;
                 var sourceVipFilePath = context.ParseResult.GetValueForOption(_options.SourceVipFilePath)!;
                 var hostLocalDataDir = context.ParseResult.GetValueForOption(_options.HostLocalDataDir)!;
+                var livenessProbePort = context.ParseResult.GetValueForOption(_options.LivenessProbePort);
 
-            retryGetNetwork:
-                var newHnsNetwork = _hnsService.GetHnsNetworks().FirstOrDefault(x => x.Name == networkName);
-                if (newHnsNetwork == null)
+                TcpListener? listener = null;
+                using var listenerCancel = CancellationTokenSource.CreateLinkedTokenSource(context.GetCancellationToken());
+                Task? listenerTask = null;
+                if (livenessProbePort.HasValue && livenessProbePort != 0)
                 {
-                    _logger.LogInformation($"Waiting for HNS network '{networkName}' to appear...");
-                    await Task.Delay(1000, context.GetCancellationToken());
-                    goto retryGetNetwork;
+                    _logger.LogInformation($"Starting liveness probe listener on port {livenessProbePort.Value}...");
+
+                    listener = new TcpListener(IPAddress.Loopback, livenessProbePort.Value);
+                    listener.Start();
+
+                    listenerTask = Task.Run(async () =>
+                    {
+                        while (!listenerCancel.IsCancellationRequested)
+                        {
+                            var client = await listener.AcceptTcpClientAsync(listenerCancel.Token);
+                            if (client == null)
+                            {
+                                continue;
+                            }
+
+                            _logger.LogInformation($"Responding to liveness probe...");
+
+                            try
+                            {
+                                using (var writer = new StreamWriter(client.GetStream(), leaveOpen: false))
+                                {
+                                    await writer.WriteLineAsync("alive");
+                                    await writer.FlushAsync(listenerCancel.Token);
+                                }
+                                client.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogWarning(ex, $"Exception when responding to liveness probe: {ex.Message}");
+                            }
+                        }
+                    });
                 }
 
-                if (hostLocalDataDir.Exists)
+                try
                 {
-                    var hostLocalNetworkDataDir = new DirectoryInfo(Path.Combine(hostLocalDataDir.FullName, networkName));
-                    if (hostLocalNetworkDataDir.Exists)
+                retryGetNetwork:
+                    var newHnsNetwork = _hnsService.GetHnsNetworks().FirstOrDefault(x => x.Name == networkName);
+                    if (newHnsNetwork == null)
                     {
-                        foreach (var file in hostLocalNetworkDataDir.GetFiles())
-                        {
-                            if (file.Name != "lock" && !file.Name.StartsWith("last_reserved_ip", StringComparison.Ordinal))
-                            {
-                                var lines = await File.ReadAllLinesAsync(file.FullName, context.GetCancellationToken());
-                                if (lines.Length >= 2)
-                                {
-                                    var readContainerId = lines[0];
-                                    var ifName = lines[1];
+                        _logger.LogInformation($"Waiting for HNS network '{networkName}' to appear...");
+                        await Task.Delay(1000, context.GetCancellationToken());
+                        goto retryGetNetwork;
+                    }
 
-                                    if (readContainerId == containerId && ifName == "source-vip")
+                    if (hostLocalDataDir.Exists)
+                    {
+                        var hostLocalNetworkDataDir = new DirectoryInfo(Path.Combine(hostLocalDataDir.FullName, networkName));
+                        if (hostLocalNetworkDataDir.Exists)
+                        {
+                            foreach (var file in hostLocalNetworkDataDir.GetFiles())
+                            {
+                                if (file.Name != "lock" && !file.Name.StartsWith("last_reserved_ip", StringComparison.Ordinal))
+                                {
+                                    var lines = await File.ReadAllLinesAsync(file.FullName, context.GetCancellationToken());
+                                    if (lines.Length >= 2)
                                     {
-                                        var sourceVipAddress = file.Name;
-                                        _logger.LogInformation($"Read existing source VIP for '{containerId}': {sourceVipAddress}");
-                                        if (sourceVipFilePath != null)
+                                        var readContainerId = lines[0];
+                                        var ifName = lines[1];
+
+                                        if (readContainerId == containerId && ifName == "source-vip")
                                         {
-                                            var sourceVipDirectoryPath = Path.GetDirectoryName(sourceVipFilePath.FullName);
-                                            if (sourceVipDirectoryPath != null)
+                                            var sourceVipAddress = file.Name;
+                                            _logger.LogInformation($"Read existing source VIP for '{containerId}': {sourceVipAddress}");
+                                            if (sourceVipFilePath != null)
                                             {
-                                                Directory.CreateDirectory(sourceVipDirectoryPath);
+                                                var sourceVipDirectoryPath = Path.GetDirectoryName(sourceVipFilePath.FullName);
+                                                if (sourceVipDirectoryPath != null)
+                                                {
+                                                    Directory.CreateDirectory(sourceVipDirectoryPath);
+                                                }
+                                                File.WriteAllText(sourceVipFilePath.FullName, sourceVipAddress);
+                                                _logger.LogInformation($"Wrote source VIP file to: {sourceVipFilePath.FullName}");
                                             }
-                                            File.WriteAllText(sourceVipFilePath.FullName, sourceVipAddress);
-                                            _logger.LogInformation($"Wrote source VIP file to: {sourceVipFilePath.FullName}");
+                                            return 0;
                                         }
-                                        return 0;
                                     }
                                 }
                             }
                         }
                     }
-                }
 
-                _logger.LogInformation($"No source VIP for '{containerId}' reserved; creating one...");
+                    _logger.LogInformation($"No source VIP for '{containerId}' reserved; creating one...");
 
-                var configData =
-                    $$"""
-                    {
-                    	"cniVersion": "1.0.0",
-                    	"name": "{{networkName}}",
-                    	"ipam": {
-                    		"type": "host-local",
-                    		"ranges": [[{"subnet":"{{newHnsNetwork.Subnets[0].AddressPrefix}}"}]],
-                    		"dataDir": "{{hostLocalDataDir.FullName.Replace("\\", "\\\\", StringComparison.Ordinal)}}"
-                    	}
-                    }
-                    """;
-
-                _logger.LogInformation($"Acquiring source VIP address using 'host-local.exe'...");
-                var stdout = new StringBuilder();
-                var stderr = new StringBuilder();
-                var exitCode = await _processExecutor.ExecuteAsync(
-                    new ProcessSpecification
-                    {
-                        FilePath = Path.Combine(cniBinPath.FullName, "host-local.exe"),
-                        Arguments = [],
-                        EnvironmentVariables = new Dictionary<string, string>
+                    var configData =
+                        $$"""
                         {
-                            { "CNI_COMMAND", "ADD" },
-                            { "CNI_CONTAINERID", containerId },
-                            { "CNI_NETNS", "kube-proxy" },
-                            { "CNI_IFNAME", "source-vip" },
-                            { "CNI_PATH", cniBinPath.FullName },
+                            "cniVersion": "1.0.0",
+                            "name": "{{networkName}}",
+                            "ipam": {
+                                "type": "host-local",
+                                "ranges": [[{"subnet":"{{newHnsNetwork.Subnets[0].AddressPrefix}}"}]],
+                                "dataDir": "{{hostLocalDataDir.FullName.Replace("\\", "\\\\", StringComparison.Ordinal)}}"
+                            }
                         }
-                    },
-                    new HostLocalCaptureSpecification(configData, stdout, stderr),
-                    context.GetCancellationToken());
-                if (exitCode != 0)
-                {
-                    _logger.LogError(
-                        $"""
-                        'host-local.exe' returned non-zero exit code:
+                        """;
 
-                        Standard output:
-                        {stdout.ToString()}
+                    _logger.LogInformation($"Acquiring source VIP address using 'host-local.exe'...");
+                    var stdout = new StringBuilder();
+                    var stderr = new StringBuilder();
+                    var exitCode = await _processExecutor.ExecuteAsync(
+                        new ProcessSpecification
+                        {
+                            FilePath = Path.Combine(cniBinPath.FullName, "host-local.exe"),
+                            Arguments = [],
+                            EnvironmentVariables = new Dictionary<string, string>
+                            {
+                                { "CNI_COMMAND", "ADD" },
+                                { "CNI_CONTAINERID", containerId },
+                                { "CNI_NETNS", "kube-proxy" },
+                                { "CNI_IFNAME", "source-vip" },
+                                { "CNI_PATH", cniBinPath.FullName },
+                            }
+                        },
+                        new HostLocalCaptureSpecification(configData, stdout, stderr),
+                        context.GetCancellationToken());
+                    if (exitCode != 0)
+                    {
+                        _logger.LogError(
+                            $"""
+                            'host-local.exe' returned non-zero exit code:
 
-                        Standard error:
-                        {stderr.ToString()}
-                        """);
+                            Standard output:
+                            {stdout.ToString()}
+
+                            Standard error:
+                            {stderr.ToString()}
+                            """);
+                        return 1;
+                    }
+
+                    try
+                    {
+                        var sourceVipResponse = JsonSerializer.Deserialize(
+                            stdout.ToString(),
+                            SourceVipResponseJsonSerializerContext.Default.SourceVipResponse);
+                        if (sourceVipResponse != null && sourceVipResponse.IPs.Length > 0)
+                        {
+                            var sourceVipAddress = sourceVipResponse.IPs[0].Address.Split('/')[0];
+                            _logger.LogInformation($"Allocated or reused source VIP address: {sourceVipAddress}");
+                            if (sourceVipFilePath != null)
+                            {
+                                var sourceVipDirectoryPath = Path.GetDirectoryName(sourceVipFilePath.FullName);
+                                if (sourceVipDirectoryPath != null)
+                                {
+                                    Directory.CreateDirectory(sourceVipDirectoryPath);
+                                }
+                                File.WriteAllText(sourceVipFilePath.FullName, sourceVipAddress);
+                                _logger.LogInformation($"Wrote source VIP file to: {sourceVipFilePath.FullName}");
+                            }
+                            return 0;
+                        }
+                        else
+                        {
+                            _logger.LogError("No source VIP reserved.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, $"Failed to unmarshal source VIP response: {ex}");
+                    }
                     return 1;
                 }
-
-                try
+                finally
                 {
-                    var sourceVipResponse = JsonSerializer.Deserialize(
-                        stdout.ToString(),
-                        SourceVipResponseJsonSerializerContext.Default.SourceVipResponse);
-                    if (sourceVipResponse != null && sourceVipResponse.IPs.Length > 0)
+                    if (listener != null)
                     {
-                        var sourceVipAddress = sourceVipResponse.IPs[0].Address.Split('/')[0];
-                        _logger.LogInformation($"Allocated or reused source VIP address: {sourceVipAddress}");
-                        if (sourceVipFilePath != null)
+                        listener.Stop();
+                        listenerCancel.Cancel();
+                        try
                         {
-                            var sourceVipDirectoryPath = Path.GetDirectoryName(sourceVipFilePath.FullName);
-                            if (sourceVipDirectoryPath != null)
-                            {
-                                Directory.CreateDirectory(sourceVipDirectoryPath);
-                            }
-                            File.WriteAllText(sourceVipFilePath.FullName, sourceVipAddress);
-                            _logger.LogInformation($"Wrote source VIP file to: {sourceVipFilePath.FullName}");
+                            await listenerTask!;
                         }
-                        return 0;
+                        catch
+                        {
+                        }
                     }
                     else
                     {
-                        _logger.LogError("No source VIP reserved.");
+                        listenerCancel.Cancel();
                     }
                 }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, $"Failed to unmarshal source VIP response: {ex}");
-                }
-                return 1;
             }
 
             private class HostLocalCaptureSpecification : ICaptureSpecification
