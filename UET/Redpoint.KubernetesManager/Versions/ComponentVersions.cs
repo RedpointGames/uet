5a53abe2dacbaedd1dd48366c61c1d4145da1f11
June Rhodes <jrhodes@redpoint.games>
2025-12-21 19:23:42 +1100

    Update default RKM component versions

--- UET/Redpoint.KubernetesManager/Versions/ComponentVersions.cs
+++ UET/Redpoint.KubernetesManager/Versions/ComponentVersions.cs
@@ -9,12 +9,12 @@
     // @note: This will move to configs in the future.
     internal class ComponentVersions
     {
-        public static string Containerd = "2.1.4";
-        public static string Runc = "1.3.0";
-        public static string Kubernetes = "1.33.3";
-        public static string Etcd = "3.6.4";
-        public static string CniPlugins = "1.7.1";
-        public static string FlannelCniSuffix = "-flannel2";
-        public static string Flannel = "0.27.2";
+        public static string Containerd = "2.2.1";
+        public static string Runc = "1.3.4";
+        public static string Kubernetes = "1.35.0";
+        public static string Etcd = "3.6.7";
+        public static string CniPlugins = "1.9.0";
+        public static string FlannelCniSuffix = "-flannel1";
+        public static string Flannel = "0.27.4";
     }
 }
