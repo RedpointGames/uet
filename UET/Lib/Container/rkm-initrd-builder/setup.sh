361d5be62e0a9900b3b2269377ed95573fd51b86
June Rhodes <jrhodes@redpoint.games>
2025-12-24 23:47:40 +1100

    more wip

--- UET/Lib/Container/rkm-initrd-builder/setup.sh
+++ UET/Lib/Container/rkm-initrd-builder/setup.sh
@@ -8,14 +8,32 @@ if [ "$TARGET_DIR" == "" ]; then
   exit 1
 fi
 
+echo '#!/bin/bash' > $TARGET_DIR/usr/sbin/swapoff
+chmod a+x $TARGET_DIR/usr/sbin/swapoff
+
 cat >$TARGET_DIR/usr/lib/systemd/system/rkm-linux.target <<EOF
 [Unit]
 Description=RKM Minimal Linux Target
 Requires=basic.target network.target
-Wants=dbus.service rkm-shell.service
+Wants=dbus.service rkm-shell.service rkm-install.service
 Conflicts=multi-user.target rescue.service rescue.target
 After=multi-user.target rescue.service rescue.target
 AllowIsolate=yes
+EOF
+
+cat >$TARGET_DIR/usr/lib/systemd/system/rkm-install.service <<EOF
+[Unit]
+Description=RKM Install
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/uet upgrade --then cluster start --auto-upgrade
+StandardInput=tty
+StandardOutput=tty
+TTYPath=/dev/tty1
+
+[Install]
+WantedBy=rkm-linux.target
 EOF
 
 cat >$TARGET_DIR/usr/lib/systemd/system/rkm-shell.service <<EOF
@@ -43,4 +61,8 @@ Type=ether
 
 [Network]
 DHCP=yes
+DNS=1.1.1.1
+DNS=1.0.0.1
+DNS=8.8.8.8
+DNS=8.8.4.4
 EOF
