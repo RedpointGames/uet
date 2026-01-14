3a6a36a29d37ee90dd4b06889c402bc89080751c
June Rhodes <jrhodes@redpoint.games>
2026-01-14 13:39:59 +1100

    Enable SSH, VNC and headless X for RKM provisioning (#477)
    The X server will fail to start if a bare metal machine has no monitors plugged in, which causes provisioning not to occur. This changes the xorg.service to run Xvfb instead if a real X server fails to start, and configures SSH to run with VNC available through an SSH port forward.

--- UET/Lib/Container/rkm-initrd-builder/setup.sh
+++ UET/Lib/Container/rkm-initrd-builder/setup.sh
@@ -10,3 +10,11 @@ fi
 
 chmod a-x $TARGET_DIR/usr/lib/systemd/system/*.service
 chmod a-x $TARGET_DIR/usr/lib/systemd/system/*.target
+chmod a-x $TARGET_DIR/usr/lib/systemd/system/*.mount
+chmod a-x $TARGET_DIR/usr/lib/systemd/network/ethernet.network
+chmod a-x $TARGET_DIR/usr/share/background-x11*
+chmod a-x $TARGET_DIR/rkm-initrd
+
+chmod u=rw,go= $TARGET_DIR/etc/ssh/ssh_host_*
+chmod a-x $TARGET_DIR/etc/ssh/sshd_config
+chmod a-x $TARGET_DIR/etc/motd
