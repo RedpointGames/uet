d5258b1162555643b3db5aaa85d1bfb657247c00
zobbus1 <m.arkciliavincenti@gmail.com>
2026-02-15 13:11:44 +0100

    Update KeyedSemaphoresVfsLocks.cs

--- UET/Redpoint.Vfs.Layer.Scratch/KeyedSemaphoresVfsLocks.cs
+++ UET/Redpoint.Vfs.Layer.Scratch/KeyedSemaphoresVfsLocks.cs
@@ -1,9 +1,9 @@
-﻿namespace Redpoint.Vfs.Layer.Scratch
+namespace Redpoint.Vfs.Layer.Scratch
 {
     using AsyncKeyedLock;
     using System.Collections.Concurrent;
 
-    internal sealed class KeyedSemaphoresVfsLocks : IVfsLocks
+    internal sealed class KeyedSemaphoresVfsLocks : IVfsLocks, IDisposable
     {
         private readonly StripedAsyncKeyedLocker<string> _locks;
         private readonly ConcurrentDictionary<string, string> _lockHolders;
@@ -14,15 +14,20 @@
             _lockHolders = new ConcurrentDictionary<string, string>();
         }
 
+        public void Dispose()
+        {
+            _locks.Dispose();
+        }
+
         public bool TryLock(string context, string normalizedKeyPath, TimeSpan timeout, Action callback, out string blockingContext)
         {
             blockingContext = "(unknown)";
-            if (!_locks.TryLock(normalizedKeyPath, timeout, () =>
+            if (!_locks.TryLock(normalizedKeyPath, () =>
             {
                 _lockHolders.AddOrUpdate(normalizedKeyPath, context, (_, _) => context);
                 callback();
                 _lockHolders.AddOrUpdate(normalizedKeyPath, "(none)", (_, _) => "(none)");
-            }))
+            }, timeout))
             {
                 _lockHolders.TryGetValue(normalizedKeyPath, out blockingContext!);
                 return false;
