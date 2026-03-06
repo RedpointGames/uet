08c19a4b33c955dd904ddf97641755a896a08e47
June Rhodes <jrhodes@redpoint.games>
2026-03-06 15:26:35 +1100

    Move common component logic to DownstreamComponent

--- UET/Redpoint.KubernetesManager/Components/DownstreamService/ContainerdComponent.cs
+++ UET/Redpoint.KubernetesManager/Components/DownstreamService/ContainerdComponent.cs
@@ -1,147 +1,36 @@
 ﻿namespace Redpoint.KubernetesManager.Components.DownstreamService
 {
-    using Redpoint.KubernetesManager.Models;
-    using Redpoint.KubernetesManager.Services.Windows;
-    using Redpoint.KubernetesManager.Signalling;
-    using System;
-    using System.Diagnostics;
-    using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
     using Redpoint.ServiceControl;
     using Redpoint.KubernetesManager.Abstractions;
     using Redpoint.KubernetesManager;
-    using Redpoint.KubernetesManager.Components;
 
     /// <summary>
     /// The containerd component sets up and runs the containerd process.
     /// </summary>
-    internal class ContainerdComponent : IComponent, IDisposable
+    internal class ContainerdComponent : DownstreamComponent
     {
-        private readonly IServiceControl _serviceControl;
-        private readonly IRkmVersionProvider _rkmVersionProvider;
-        private readonly IPathProvider _pathProvider;
-        private readonly ILogger<ContainerdComponent> _logger;
-        private readonly CancellationTokenSource _stoppingToken;
-        private Task? _logTask;
-
         public ContainerdComponent(
             IServiceControl serviceControl,
             IRkmVersionProvider rkmVersionProvider,
             IPathProvider pathProvider,
             ILogger<ContainerdComponent> logger)
+            : base(
+                serviceControl,
+                rkmVersionProvider,
+                pathProvider,
+                logger)
         {
-            _serviceControl = serviceControl;
-            _rkmVersionProvider = rkmVersionProvider;
-            _pathProvider = pathProvider;
-            _logger = logger;
-            _stoppingToken = new CancellationTokenSource();
         }
 
-        public void Dispose()
-        {
-            ((IDisposable)_stoppingToken).Dispose();
-        }
+        protected override string ServiceName => "rkm-containerd";
 
-        public void RegisterSignals(IRegistrationContext context)
-        {
-            context.OnSignal(WellKnownSignals.Started, OnStartedAsync);
-            context.OnSignal(WellKnownSignals.Stopping, OnStoppingAsync);
-        }
+        protected override string ServiceDescription => "RKM - Containerd";
 
-        private const string _serviceName = "rkm-containerd";
+        protected override string RunCommand => "run-containerd";
 
-        private async Task OnStartedAsync(IContext context, IAssociatedData? data, CancellationToken cancellationToken)
-        {
-            await context.WaitForFlagAsync(WellKnownFlags.OSNetworkingReady);
+        protected override string ManifestFileName => "containerd-manifest.json";
 
-            Directory.CreateDirectory(Path.Combine(_pathProvider.RKMRoot, "cache"));
-
-            var arguments = $"\"{_rkmVersionProvider.UetGenericFilePath}\" cluster run-containerd --manifest-path \"{Path.Combine(_pathProvider.RKMRoot, "cache", "containerd-manifest.json")}\"";
-
-            var installed = false;
-            if (await _serviceControl.IsServiceInstalled(_serviceName))
-            {
-                var result = await _serviceControl.GetServiceExecutableAndArguments(_serviceName);
-                if (result == arguments)
-                {
-                    _logger.LogInformation("containerd service is already installed correctly.");
-                    installed = true;
-                }
-                else
-                {
-                    if (await _serviceControl.IsServiceRunning(_serviceName, cancellationToken))
-                    {
-                        _logger.LogInformation("containerd service is being stopped so it can be reinstalled.");
-                        await _serviceControl.StopService(_serviceName, cancellationToken);
-                    }
-
-                    _logger.LogInformation("containerd service is being uninstalled because the command line arguments need to change.");
-                    await _serviceControl.UninstallService(_serviceName);
-                }
-            }
-
-            if (!installed)
-            {
-                await _serviceControl.InstallService(
-                    _serviceName,
-                    "RKM - Containerd",
-                    arguments,
-                    manualStart: true);
-            }
-
-            if (Debugger.IsAttached)
-            {
-                _logTask = _serviceControl.StreamLogsUntilCancelledAsync(
-                    _serviceName,
-                    (level, message) =>
-                    {
-                        switch (level)
-                        {
-                            case ServiceLogLevel.Information:
-                                _logger.LogInformation($"(containerd) {message}");
-                                break;
-                            case ServiceLogLevel.Warning:
-                                _logger.LogWarning($"(containerd) {message}");
-                                break;
-                            case ServiceLogLevel.Error:
-                                _logger.LogError($"(containerd) {message}");
-                                break;
-                            default:
-                                _logger.LogInformation($"(containerd) {message}");
-                                break;
-                        }
-                    },
-                    _stoppingToken.Token);
-            }
-
-            if (!await _serviceControl.IsServiceRunning(_serviceName, cancellationToken))
-            {
-                _logger.LogInformation("containerd service is being started...");
-                await _serviceControl.StartService(_serviceName, cancellationToken);
-            }
-        }
-
-        private async Task OnStoppingAsync(IContext context, IAssociatedData? data, CancellationToken cancellationToken)
-        {
-            if (await _serviceControl.IsServiceInstalled(_serviceName))
-            {
-                _logger.LogInformation("containerd service is being stopped...");
-                await _serviceControl.StopService(_serviceName, cancellationToken);
-            }
-
-            if (_logTask != null && !_stoppingToken.IsCancellationRequested)
-            {
-                _stoppingToken.Cancel();
-
-                try
-                {
-                    await _logTask;
-                }
-                catch (OperationCanceledException)
-                {
-                }
-            }
-        }
+        protected override string DisplayName => "containerd";
     }
 }
---
+++ UET/Redpoint.KubernetesManager/Components/DownstreamService/DownstreamComponent.cs
@@ -0,0 +1,147 @@
+﻿namespace Redpoint.KubernetesManager.Components.DownstreamService
+{
+    using Microsoft.Extensions.Logging;
+    using Redpoint.KubernetesManager.Abstractions;
+    using Redpoint.KubernetesManager.Signalling;
+    using Redpoint.ServiceControl;
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    internal abstract class DownstreamComponent : IComponent, IDisposable
+    {
+        private readonly IServiceControl _serviceControl;
+        private readonly IRkmVersionProvider _rkmVersionProvider;
+        private readonly IPathProvider _pathProvider;
+        private readonly ILogger _logger;
+        private readonly CancellationTokenSource _stoppingToken;
+        private Task? _logTask;
+
+        public DownstreamComponent(
+            IServiceControl serviceControl,
+            IRkmVersionProvider rkmVersionProvider,
+            IPathProvider pathProvider,
+            ILogger logger)
+        {
+            _serviceControl = serviceControl;
+            _rkmVersionProvider = rkmVersionProvider;
+            _pathProvider = pathProvider;
+            _logger = logger;
+            _stoppingToken = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)_stoppingToken).Dispose();
+        }
+
+        public void RegisterSignals(IRegistrationContext context)
+        {
+            context.OnSignal(WellKnownSignals.Started, OnStartedAsync);
+            context.OnSignal(WellKnownSignals.Stopping, OnStoppingAsync);
+        }
+
+        protected abstract string ServiceName { get; }
+
+        protected abstract string ServiceDescription { get; }
+
+        protected abstract string RunCommand { get; }
+
+        protected abstract string ManifestFileName { get; }
+
+        protected abstract string DisplayName { get; }
+
+        private async Task OnStartedAsync(IContext context, IAssociatedData? data, CancellationToken cancellationToken)
+        {
+            await context.WaitForFlagAsync(WellKnownFlags.OSNetworkingReady);
+
+            Directory.CreateDirectory(Path.Combine(_pathProvider.RKMRoot, "cache"));
+
+            var arguments = $"\"{_rkmVersionProvider.UetGenericFilePath}\" cluster {RunCommand} --manifest-path \"{Path.Combine(_pathProvider.RKMRoot, "cache", ManifestFileName)}\"";
+
+            var installed = false;
+            if (await _serviceControl.IsServiceInstalled(ServiceName))
+            {
+                var result = await _serviceControl.GetServiceExecutableAndArguments(ServiceName);
+                if (result == arguments)
+                {
+                    _logger.LogInformation($"{DisplayName} service is already installed correctly.");
+                    installed = true;
+                }
+                else
+                {
+                    if (await _serviceControl.IsServiceRunning(ServiceName, cancellationToken))
+                    {
+                        _logger.LogInformation($"{DisplayName} service is being stopped so it can be reinstalled.");
+                        await _serviceControl.StopService(ServiceName, cancellationToken);
+                    }
+
+                    _logger.LogInformation($"{DisplayName} service is being uninstalled because the command line arguments need to change.");
+                    await _serviceControl.UninstallService(ServiceName);
+                }
+            }
+
+            if (!installed)
+            {
+                await _serviceControl.InstallService(
+                    ServiceName,
+                    ServiceDescription,
+                    arguments,
+                    manualStart: true);
+            }
+
+            if (Debugger.IsAttached)
+            {
+                _logTask = _serviceControl.StreamLogsUntilCancelledAsync(
+                    ServiceName,
+                    (level, message) =>
+                    {
+                        switch (level)
+                        {
+                            case ServiceLogLevel.Information:
+                                _logger.LogInformation($"({DisplayName}) {message}");
+                                break;
+                            case ServiceLogLevel.Warning:
+                                _logger.LogWarning($"({DisplayName}) {message}");
+                                break;
+                            case ServiceLogLevel.Error:
+                                _logger.LogError($"({DisplayName}) {message}");
+                                break;
+                            default:
+                                _logger.LogInformation($"({DisplayName}) {message}");
+                                break;
+                        }
+                    },
+                    _stoppingToken.Token);
+            }
+
+            if (!await _serviceControl.IsServiceRunning(ServiceName, cancellationToken))
+            {
+                _logger.LogInformation($"{DisplayName} service is being started...");
+                await _serviceControl.StartService(ServiceName, cancellationToken);
+            }
+        }
+
+        private async Task OnStoppingAsync(IContext context, IAssociatedData? data, CancellationToken cancellationToken)
+        {
+            if (await _serviceControl.IsServiceInstalled(ServiceName))
+            {
+                _logger.LogInformation($"{DisplayName} service is being stopped...");
+                await _serviceControl.StopService(ServiceName, cancellationToken);
+            }
+
+            if (_logTask != null && !_stoppingToken.IsCancellationRequested)
+            {
+                _stoppingToken.Cancel();
+
+                try
+                {
+                    await _logTask;
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            }
+        }
+    }
+}
