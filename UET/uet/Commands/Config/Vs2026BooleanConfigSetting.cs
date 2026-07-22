6d0f2b6a5ec3ca6f09aeb6e7e228032e906edb04
June Rhodes <jrhodes@redpoint.games>
2026-07-22 22:56:56 +1000

    Add vs2026 config option (#687)

---
+++ UET/uet/Commands/Config/Vs2026BooleanConfigSetting.cs
@@ -0,0 +1,69 @@
+﻿namespace UET.Commands.Config
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    internal sealed class Vs2026BooleanConfigSetting : IBooleanConfigSetting
+    {
+        private readonly IXmlConfigHelper _configHelper;
+
+        public Vs2026BooleanConfigSetting(IXmlConfigHelper configHelper)
+        {
+            _configHelper = configHelper;
+        }
+
+        public string Name => "vs2026";
+
+        public string Description => "When enabled, project files will be generated for Visual Studio 2026 instead of 2022.";
+
+        private static readonly string _xmlConfigFilePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Unreal Engine",
+            "UnrealBuildTool",
+            "BuildConfiguration.xml");
+
+        public Task<bool> GetValueAsync(CancellationToken cancellationToken)
+        {
+            if (!File.Exists(_xmlConfigFilePath))
+            {
+                return Task.FromResult(false);
+            }
+
+            var document = new XmlDocument();
+            document.Load(_xmlConfigFilePath);
+
+            var value = _configHelper.GetValue(document, ["Configuration", "VCProjectFileGenerator", "Version"]);
+
+            return Task.FromResult(value == "VisualStudio2026");
+        }
+
+        public Task SetValueAsync(bool value, CancellationToken cancellationToken)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(_xmlConfigFilePath)!);
+
+            var document = new XmlDocument();
+            if (File.Exists(_xmlConfigFilePath))
+            {
+                document.Load(_xmlConfigFilePath);
+            }
+            else
+            {
+                document.AppendChild(document.CreateXmlDeclaration("1.0", "utf-8", null));
+            }
+
+            if (value)
+            {
+                _configHelper.SetValue(document, ["Configuration", "VCProjectFileGenerator", "Version"], "VisualStudio2026");
+            }
+            else
+            {
+                _configHelper.DeleteValue(document, ["Configuration", "VCProjectFileGenerator", "Version"]);
+            }
+
+            document.Save(_xmlConfigFilePath);
+
+            return Task.CompletedTask;
+        }
+    }
+}
