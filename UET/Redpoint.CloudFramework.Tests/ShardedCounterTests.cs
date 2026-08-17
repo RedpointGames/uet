dc3fb4cf0fd627a9f204a3c3759c2278f43332b6
June Rhodes <jrhodes@redpoint.games>
2026-08-17 17:00:22 +1000

    Attempt to fix Redis race condition in sharded counters
    I suspect there a race condition in the sharded counter logic wrt Redis caching. I believe it happens due to AdjustAsync calling StringIncrementAsync, followed by GetAsync only updating the cache if no value is present. This means if the existing Redis key expires between GetAsync and AdjustAsync, AdjustAsync will increment an unset value (which is treated as 0 if it doesn't exist).
    
    However, it's not as simple as adding a "does the key exist" check on AdjustAsync, because there's in theory another race condition that can happen with two processes running GetAsync and AdjustAsync at the same time:
    
    - GetAsync sees there is no key
    - GetAsync loads from Datastore without a transaction
    - AdjustAsync increments values in Datastore
    - AdjustAsync does not write into Redis because the key is missing
    - GetAsync writes the stale value into Redis
    - Now the Redis cache is off by whatever amount AdjustAsync changed
    
    So to solve this, we need GetAsync to store a value in Redis that says "I'm about to do a fetch-store operation" if the key doesn't exist, and have AdjustAsync clear that when it runs. Then GetAsync needs to check that it's fetch-store operation flag is still present in Redis when it stores the cached value. This will allow AdjustAsync to prevent any in-flight fetch-store operations from storing stale values.

--- UET/Redpoint.CloudFramework.Tests/ShardedCounterTests.cs
+++ UET/Redpoint.CloudFramework.Tests/ShardedCounterTests.cs
@@ -4,11 +4,13 @@
     using Microsoft.Extensions.DependencyInjection;
     using Redpoint.CloudFramework.Counter;
     using Redpoint.CloudFramework.Repository;
+    using StackExchange.Redis;
     using System;
     using System.Diagnostics;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using System.Xml.Linq;
     using Xunit;
 
     [Collection("CloudFramework Test")]
@@ -113,6 +115,123 @@
 
             Debug.WriteLine($"Took {stopwatch.ElapsedMilliseconds} ms to get all counters.");
 #pragma warning restore CA5394
+        }
+
+        [Fact]
+        public async Task TestLuaScriptsForRedisCaching()
+        {
+            var redisDatabase = _env.Services.GetRequiredService<IConnectionMultiplexer>().GetDatabase();
+
+            var fetchStoreToken = Guid.NewGuid().ToString();
+
+            var keyA = new RedisKey("a");
+            var keyB = new RedisKey("b");
+            var keyC = new RedisKey("c");
+            var keyD = new RedisKey("d");
+            var keyE = new RedisKey("e");
+
+            var tokenKeyA = new RedisKey("token-a");
+            var tokenKeyB = new RedisKey("token-b");
+            var tokenKeyC = new RedisKey("token-c");
+            var tokenKeyD = new RedisKey("token-d");
+            var tokenKeyE = new RedisKey("token-e");
+
+            redisDatabase.StringSet(keyA, 0);
+            redisDatabase.KeyDelete(keyB); // keyB not set
+            redisDatabase.StringSet(keyC, 10);
+            redisDatabase.KeyDelete(keyD); // keyD not set
+            redisDatabase.StringSet(keyE, 20);
+
+            var cachedValues = DefaultGlobalShardedCounter.RedisResultToArrayOfNumbers(await redisDatabase.ScriptEvaluateAsync(
+                DefaultGlobalShardedCounter._loadExistingOrFlagFetchStoreOperation,
+                [
+                    keyA,
+                    tokenKeyA,
+                    keyB,
+                    tokenKeyB,
+                    keyC,
+                    tokenKeyC,
+                    keyD,
+                    tokenKeyD,
+                    keyE,
+                    tokenKeyE,
+                ],
+                [
+                    new RedisValue(fetchStoreToken)
+                ]));
+            Assert.Equal(
+                new long?[]
+                {
+                    0,
+                    null,
+                    10,
+                    null,
+                    20
+                },
+                cachedValues);
+
+            Assert.True(redisDatabase.StringGet(tokenKeyA).IsNull);
+            Assert.Equal(fetchStoreToken, redisDatabase.StringGet(tokenKeyB).ToString());
+            Assert.True(redisDatabase.StringGet(tokenKeyC).IsNull);
+            Assert.Equal(fetchStoreToken, redisDatabase.StringGet(tokenKeyD).ToString());
+            Assert.True(redisDatabase.StringGet(tokenKeyE).IsNull);
+
+            await redisDatabase.ScriptEvaluateAsync(
+                DefaultGlobalShardedCounter._adjustOrBreakFetchStoreOperation,
+                [
+                    keyB,
+                    tokenKeyB,
+                ],
+                [
+                    new RedisValue("200")
+                ]);
+            await redisDatabase.ScriptEvaluateAsync(
+                DefaultGlobalShardedCounter._adjustOrBreakFetchStoreOperation,
+                [
+                    keyC,
+                    tokenKeyC,
+                ],
+                [
+                    new RedisValue("200")
+                ]);
+
+            Assert.Equal("0", redisDatabase.StringGet(keyA).ToString());
+            Assert.True(redisDatabase.StringGet(keyB).IsNull);
+            Assert.Equal("210", redisDatabase.StringGet(keyC).ToString());
+            Assert.True(redisDatabase.StringGet(keyD).IsNull);
+            Assert.Equal("20", redisDatabase.StringGet(keyE).ToString());
+
+            Assert.True(redisDatabase.StringGet(tokenKeyA).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyB).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyC).IsNull);
+            Assert.Equal(fetchStoreToken, redisDatabase.StringGet(tokenKeyD).ToString());
+            Assert.True(redisDatabase.StringGet(tokenKeyE).IsNull);
+
+            await redisDatabase.ScriptEvaluateAsync(
+                DefaultGlobalShardedCounter._storeFetchedValueIfFetchStoreOperationUnbroken,
+                [
+                    keyB,
+                    tokenKeyB,
+                    keyD,
+                    tokenKeyD,
+                ],
+                [
+                    fetchStoreToken,
+                    new RedisValue("5"),
+                    new RedisValue("15"),
+                ]);
+
+            Assert.Equal("0", redisDatabase.StringGet(keyA).ToString());
+            Assert.True(redisDatabase.StringGet(keyB).IsNull);
+            Assert.Equal("210", redisDatabase.StringGet(keyC).ToString());
+            Assert.Equal("15", redisDatabase.StringGet(keyD).ToString());
+            Assert.Equal("20", redisDatabase.StringGet(keyE).ToString());
+
+            Assert.True(redisDatabase.StringGet(tokenKeyA).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyB).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyC).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyD).IsNull);
+            Assert.True(redisDatabase.StringGet(tokenKeyE).IsNull);
         }
     }
 }
