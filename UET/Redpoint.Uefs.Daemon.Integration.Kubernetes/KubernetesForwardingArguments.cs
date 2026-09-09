44e0948ed702055670d88e03474c94b458e4c721
June Rhodes <jrhodes@redpoint.games>
2026-09-09 19:27:26 +1000

    Remove legacy Docker and Kubernetes code for UEFS (#743)
    This integration code either isn't wired up, or hasn't ever been used in production. In order for this code to be practically useful for Unreal Engine builds, we'd need to first have a build executor that can run builds inside Docker/Kubernetes with custom volume mounts, and there's no plan to currently implement one (running the GitLab executor inside a host-process pod works just fine).

--- UET/Redpoint.Uefs.Daemon.Integration.Kubernetes/KubernetesForwardingArguments.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Redpoint.Uefs.Daemon.Integration.Kubernetes
-{
-    internal sealed class KubernetesForwardingArguments
-    {
-        public int Port { get; set; }
-
-        public string? UnixSocketPath { get; set; }
-    }
-}
